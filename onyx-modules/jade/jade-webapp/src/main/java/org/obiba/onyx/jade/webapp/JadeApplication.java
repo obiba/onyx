201269d8b22a925e627cdfe2a27db7f1985347da
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-11 20:04:10 +0000

    Jade web application added.

---
+++ onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/JadeApplication.java
@@ -0,0 +1,13 @@
+package org.obiba.onyx.jade.webapp;
+
+import org.apache.wicket.spring.SpringWebApplication;
+
+public class JadeApplication extends SpringWebApplication {
+
+  @Override
+  public Class getHomePage() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
