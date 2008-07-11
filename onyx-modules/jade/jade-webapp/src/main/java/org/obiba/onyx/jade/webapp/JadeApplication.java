2c41d9c594366e5618b3b84cbc1066442b388007
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-11 20:30:03 +0000

    Jade Web Application activated with a Home Page.

--- onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/JadeApplication.java
+++ onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/JadeApplication.java
@@ -1,13 +1,23 @@
 package org.obiba.onyx.jade.webapp;
 
 import org.apache.wicket.spring.SpringWebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.obiba.onyx.jade.webapp.pages.home.HomePage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JadeApplication extends SpringWebApplication {
+  private final Logger log = LoggerFactory.getLogger(JadeApplication.class);
+  protected void init() {
+    log.info("Initializing Jade Web Application ");
+    super.init();
 
+    super.addComponentInstantiationListener(new SpringComponentInjector(this, super.getSpringContextLocator().getSpringContext()));
+  }
+  
   @Override
-  public Class getHomePage() {
-    // TODO Auto-generated method stub
-    return null;
+  public Class<?> getHomePage() {
+    return HomePage.class;
   }
 
 }
---
+++ onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/pages/home/HomePage.java
@@ -0,0 +1,7 @@
+package org.obiba.onyx.jade.webapp.pages.home;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+
+}
