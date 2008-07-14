0cbd83ab1a2230ae8c4a05909a4cf6356654b5a1
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-14 20:56:31 +0000

    HomePage update.

--- onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/pages/home/HomePage.java
+++ onyx-modules/jade/jade-webapp/src/main/java/org/obiba/onyx/jade/webapp/pages/home/HomePage.java
@@ -1,7 +1,31 @@
 package org.obiba.onyx.jade.webapp.pages.home;
 
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 
-public class HomePage extends WebPage {
+public class HomePage extends WebPage implements IAjaxIndicatorAware {
+
+  private FeedbackPanel feedbackPanel;
+  
+  public HomePage() {
+    super();
+    
+    //Create feedback panel and add to page
+    feedbackPanel = new FeedbackPanel("feedback");
+    feedbackPanel.setOutputMarkupId(true);
+    add(feedbackPanel);
+    
+    add(new Label("baseAjaxIndicator", "Processing..."));
+  }
+
+  /**
+   * @see org.apache.wicket.ajax.IAjaxIndicatorAware#getAjaxIndicatorMarkupId()
+   */
+  public String getAjaxIndicatorMarkupId()
+  {
+    return "base-ajax-indicator";
+  }
 
 }
