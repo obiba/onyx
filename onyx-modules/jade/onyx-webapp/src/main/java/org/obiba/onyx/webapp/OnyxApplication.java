bb2b4a7807d547375e9050d7a8e41e0e0d474791
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-07-15 19:21:54 +0000

    Removed jade-webapp in favour of onyx-webapp. Moved WebApplicationStartupListener bean definition into jade-core. Created OnyxApplication.

---
+++ onyx-modules/jade/onyx-webapp/src/main/java/org/obiba/onyx/webapp/OnyxApplication.java
@@ -0,0 +1,91 @@
+package org.obiba.onyx.webapp;
+
+import java.util.Map;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.spring.SpringWebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.obiba.onyx.webapp.pages.home.HomePage;
+import org.obiba.wicket.application.WebApplicationStartupListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnyxApplication extends SpringWebApplication {
+
+  private final Logger log = LoggerFactory.getLogger(OnyxApplication.class);
+
+  protected void init() {
+    log.info("Onyx Web Application is starting");
+    super.init();
+
+    super.addComponentInstantiationListener(new SpringComponentInjector(this, super.internalGetApplicationContext()));
+
+    forEachListeners(new IListenerCallback() {
+      public void handleListener(String beanName, WebApplicationStartupListener listener) {
+        listener.startup(OnyxApplication.this);
+      }
+
+      public boolean terminateOnException() {
+        return true;
+      }
+    });
+    log.info("Onyx Web Application has been started");
+  }
+
+  @Override
+  protected void onDestroy() {
+    log.info("Onyx Web Application is stoping");
+    forEachListeners(new IListenerCallback() {
+      public void handleListener(String beanName, WebApplicationStartupListener listener) {
+        listener.shutdown(OnyxApplication.this);
+      }
+
+      public boolean terminateOnException() {
+        return false;
+      }
+    });
+    log.info("Onyx Web Application has been stoped");
+    super.onDestroy();
+  }
+
+  @Override
+  public Class<?> getHomePage() {
+    return HomePage.class;
+  }
+
+  public boolean isDevelopmentMode() {
+    return Application.DEVELOPMENT.equalsIgnoreCase(getConfigurationType());
+  }
+
+  /**
+   * Finds implementations of {@link WebApplicationStartupListener} in Spring's Application Context and executes the
+   * specified callback for each instance found.
+   * @param callback the callback implementation to call for each listener instance
+   */
+  @SuppressWarnings("unchecked")
+  private void forEachListeners(IListenerCallback callback) {
+    Map<String, WebApplicationStartupListener> listeners = super.internalGetApplicationContext().getBeansOfType(WebApplicationStartupListener.class);
+    if(listeners != null) {
+      for(Map.Entry<String, WebApplicationStartupListener> entry : listeners.entrySet()) {
+        log.info("Executing WebApplicationStartupListener named {} of type {}", entry.getKey(), entry.getValue().getClass().getSimpleName());
+        try {
+          callback.handleListener(entry.getKey(), entry.getValue());
+        } catch(RuntimeException e) {
+          log.error("Error executing WebApplicationStartupListener named {} of type {}", entry.getKey(), entry.getValue().getClass().getSimpleName());
+          log.error("Reported error : ", e);
+          if(callback.terminateOnException()) throw e;
+        }
+      }
+    }
+  }
+
+  /**
+   * Called for each WebApplicationStartupListener found in Spring's application context. Implementations should either
+   * call startup or shutdown.
+   */
+  private interface IListenerCallback {
+    public void handleListener(String beanName, WebApplicationStartupListener listener);
+
+    public boolean terminateOnException();
+  }
+}
---
+++ onyx-modules/jade/onyx-webapp/src/main/java/org/obiba/onyx/webapp/pages/home/HomePage.java
@@ -0,0 +1,34 @@
+package org.obiba.onyx.webapp.pages.home;
+
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.obiba.onyx.jade.core.wicket.panel.JadePanel;
+
+public class HomePage extends WebPage implements IAjaxIndicatorAware {
+
+  private FeedbackPanel feedbackPanel;
+  
+  public HomePage() {
+    super();
+    
+    //Create feedback panel and add to page
+    feedbackPanel = new FeedbackPanel("feedback");
+    feedbackPanel.setOutputMarkupId(true);
+    add(feedbackPanel);
+    
+    add(new Label("baseAjaxIndicator", "Processing..."));
+    
+    add(new JadePanel("jade"));
+  }
+
+  /**
+   * @see org.apache.wicket.ajax.IAjaxIndicatorAware#getAjaxIndicatorMarkupId()
+   */
+  public String getAjaxIndicatorMarkupId()
+  {
+    return "base-ajax-indicator";
+  }
+
+}
