8b12f014f908d3139a07332db82feede7c0ade7b
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-17 20:03:42 +0000

    Data added.

---
+++ onyx-modules/jade/onyx-util/src/main/java/org/obiba/onyx/util/data/Data.java
@@ -0,0 +1,68 @@
+package org.obiba.onyx.util.data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Data implements Serializable {
+
+  private static final long serialVersionUID = -2470483891384378865L;
+
+  private Serializable value;
+
+  private DataType type;
+
+  public Data(DataType type) {
+    this.type = type;
+  }
+
+  public Data(DataType type, Serializable value) {
+    this.type = type;
+    setValue(value);
+  }
+
+  public DataType getType() {
+    return type;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> T getValue() {
+    return (T)value;
+  }
+
+  public void setValue(Serializable value) {
+    if(value != null) {
+      
+      switch(type) {
+      case BOOLEAN:
+        if(!value.getClass().isAssignableFrom(Boolean.class)) throw new IllegalArgumentException("DataType " + type + " expected, " + value.getClass().getName() + " received.");
+        break;
+        
+      case DATE:
+        if(!value.getClass().isAssignableFrom(Date.class)) throw new IllegalArgumentException("DataType " + type + " expected, " + value.getClass().getName() + " received.");
+        break;
+        
+      case DECIMAL:
+        if(!value.getClass().isAssignableFrom(Double.class) || !value.getClass().isAssignableFrom(Float.class)) throw new IllegalArgumentException("DataType " + type + " expected, " + value.getClass().getName() + " received.");
+        
+        break;
+        
+      case INTEGER:
+        if(!value.getClass().isAssignableFrom(Long.class) || !value.getClass().isAssignableFrom(Integer.class)) throw new IllegalArgumentException("DataType " + type + " expected, " + value.getClass().getName() + " received.");
+        break;
+        
+      case TEXT:
+        if(!value.getClass().isAssignableFrom(String.class)) throw new IllegalArgumentException("DataType " + type + " expected, " + value.getClass().getName() + " received.");
+        break;
+        
+      case DATA:
+        
+        break;
+
+      default:
+        break;
+      }
+    }
+    this.value = value;
+  }
+
+}
