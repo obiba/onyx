e77ed21020c0aa2a997e8f291626b802715c3a9a
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-22 19:59:51 +0000

    Application context for InstrumentLauncherPanel test added.

--- onyx-modules/jade/jade-core/src/test/java/org/obiba/onyx/jade/core/wicket/panel/InstrumentLauncherPanelTest.java
+++ onyx-modules/jade/jade-core/src/test/java/org/obiba/onyx/jade/core/wicket/panel/InstrumentLauncherPanelTest.java
@@ -1,36 +1,43 @@
 package org.obiba.onyx.jade.core.wicket.panel;
 
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.TestPanelSource;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.obiba.onyx.jade.core.domain.instrument.InstrumentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = "")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "InstrumentLauncherPanelTest.xml")
 public class InstrumentLauncherPanelTest {
 
-//  @Autowired
+  @Autowired
   WebApplication application;
 
   WicketTester tester;
 
   @Before
   public void setup() {
-//    tester = new WicketTester(application);
+    tester = new WicketTester(application);
   }
 
   @Test
   public void testBlah() {
-//    tester.startPanel(new TestPanelSource() {
-//      public Panel getTestPanel(String panelId) {
-//        return null;
-//      }
-//    });
+    tester.startPanel(new TestPanelSource() {
+      public Panel getTestPanel(String panelId) {
+        return new InstrumentLauncherPanel(panelId, new Model(new InstrumentType()));
+      }
+    });
+    tester.dumpPage();
+    FormTester formTester = tester.newFormTester("panel:form");
+    formTester.submit();
+    tester.assertNoErrorMessage();
   }
 }
