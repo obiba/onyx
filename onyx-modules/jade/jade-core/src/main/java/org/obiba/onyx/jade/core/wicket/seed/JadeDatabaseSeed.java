195130641c982a09380f7e92a4c3797858351ee0
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-23 18:26:24 +0000

    Spring way of setting xstream resources to JadeDatabaseSeed.

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/seed/JadeDatabaseSeed.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/seed/JadeDatabaseSeed.java
@@ -1,8 +1,6 @@
 package org.obiba.onyx.jade.core.wicket.seed;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.protocol.http.WebApplication;
@@ -19,34 +17,17 @@ import org.obiba.onyx.jade.core.service.InstrumentService;
 import org.obiba.wicket.util.seed.XstreamResourceDatabaseSeed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.Resource;
 
 import com.thoughtworks.xstream.XStream;
 
-public class JadeDatabaseSeed extends XstreamResourceDatabaseSeed implements ApplicationContextAware {
+public class JadeDatabaseSeed extends XstreamResourceDatabaseSeed {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
-
-  private static final String INSTRUMENT_TYPES_XML = "instrumentTypes-descriptor.xml";
-
-  private static final String MANUAL_INSTRUMENTS_XML = "manual-instruments-descriptor.xml";
-
-  private static final String INSTRUMENT_XML = "lib/instrument-descriptor.xml";
 
   private PersistenceManager persistenceManager;
 
   private InstrumentService instrumentService;
-
-  private ApplicationContext applicationContext;
-
-  private Resource instrumentsResource;
-  
-  private String instrumentsDir;
-
-  private boolean notSeeded = true;
 
   public void setPersistenceManager(PersistenceManager persistenceManager) {
     this.persistenceManager = persistenceManager;
@@ -55,54 +36,14 @@ public class JadeDatabaseSeed extends XstreamResourceDatabaseSeed implements App
   public void setInstrumentService(InstrumentService instrumentService) {
     this.instrumentService = instrumentService;
   }
-
-  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-    this.applicationContext = applicationContext;
-  }
-
-  public void setInstrumentsDir(String instrumentsDir) {
-    this.instrumentsDir = instrumentsDir;
-  }
-
-  @Override
-  public void seedDatabase(WebApplication application) {
-    notSeeded = (persistenceManager.list(InstrumentType.class).size() == 0);
-
-    instrumentsResource = getResource();
-
-    try {
-      List<Resource> xmlResources = new ArrayList<Resource>();
-      
-      // add resources for describig types and manual instruments
-      xmlResources.add(instrumentsResource.createRelative(INSTRUMENT_TYPES_XML));
-      xmlResources.add(instrumentsResource.createRelative(MANUAL_INSTRUMENTS_XML));
-
-      // look for instruments descriptor in instrument packages
-      Resource root = applicationContext.getResource(instrumentsDir + "/");
-      for(File instDir : root.getFile().listFiles()) {
-        Resource res = root.createRelative(instDir.getName() + "/" + INSTRUMENT_XML);
-        if(res.exists()) xmlResources.add(res);
-      }
-
-      log.debug("instrument resources: " + xmlResources);
-
-      for(Resource res : xmlResources) {
-        setResource(res);
-        super.seedDatabase(application);
-      }
-
-    } catch(IOException e) {
-      log.error("Failed loading instruments resource", e);
-    }
-  }
-
+  
   @SuppressWarnings("unchecked")
   @Override
-  protected void handleXstreamResult(Object result) {
+  protected void handleXstreamResult(Resource resource, Object result) {
     if(result != null && result instanceof List) {
       List<Object> objects = (List<Object>) result;
       for(Object entity : objects) {
-        log.info("Seeding database with entity {} of type {}", entity, entity.getClass().getSimpleName());
+        log.info("Seeding database from [" + resource + "] with entity {} of type {}", entity, entity.getClass().getSimpleName());
         if(entity instanceof Instrument) {
           Instrument instrument = (Instrument) entity;
           InstrumentType type = instrumentService.getInstrumentType(instrument.getInstrumentType().getName());
@@ -110,7 +51,7 @@ public class JadeDatabaseSeed extends XstreamResourceDatabaseSeed implements App
           // find code base:
           // resource is in .../<codeBase>/lib/instrument-descriptor.xml
           try {
-            instrument.setCodeBase(getResource().getFile().getParentFile().getParentFile().getName());
+            instrument.setCodeBase(resource.getFile().getParentFile().getParentFile().getName());
           } catch(IOException e) {
             // ignore, not a file based resource
           }
@@ -127,11 +68,12 @@ public class JadeDatabaseSeed extends XstreamResourceDatabaseSeed implements App
   @Override
   protected boolean shouldSeed(WebApplication application) {
     boolean seed = super.shouldSeed(application);
-    return seed && notSeeded;
+    return seed && (persistenceManager.list(InstrumentType.class).size() == 0);
   }
 
   @Override
   protected void initializeXstream(XStream xstream) {
+    log.info("initializeXstream");
     super.initializeXstream(xstream);
     xstream.alias("instrumentType", InstrumentType.class);
     xstream.alias("instrument", Instrument.class);
