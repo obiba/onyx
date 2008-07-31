2c1e6f4935b68826f502c570b8b63104d3a7619b
Yannick Marcon <yannick.marcon@obiba.org>
2008-07-31 20:04:51 +0000

    Default transition events added.

--- onyx-modules/jade/onyx-core/src/main/java/org/obiba/onyx/engine/state/TransitionEvent.java
+++ onyx-modules/jade/onyx-core/src/main/java/org/obiba/onyx/engine/state/TransitionEvent.java
@@ -1,6 +1,13 @@
 package org.obiba.onyx.engine.state;
 
 public class TransitionEvent {
+  
+  public static final TransitionEvent START = new TransitionEvent("start");
+  public static final TransitionEvent CANCEL = new TransitionEvent("cancel");
+  public static final TransitionEvent COMPLETE = new TransitionEvent("complete");
+  public static final TransitionEvent SKIP = new TransitionEvent("skip");
+  public static final TransitionEvent INTERRUPT = new TransitionEvent("interrupt");
+  public static final TransitionEvent RESUME = new TransitionEvent("resume");
 
   private String name;
 
