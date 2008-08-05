dbeb268a9bffb3c39c9378c16ee32ba26607f99a
Yannick Marcon <yannick.marcon@obiba.org>
2008-08-05 20:08:30 +0000

    Case participant is null handled.

--- onyx-modules/jade/onyx-webapp/src/main/java/org/obiba/onyx/webapp/stage/page/StagePage.java
+++ onyx-modules/jade/onyx-webapp/src/main/java/org/obiba/onyx/webapp/stage/page/StagePage.java
@@ -3,7 +3,9 @@ package org.obiba.onyx.webapp.stage.page;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.obiba.onyx.core.domain.participant.Participant;
 import org.obiba.onyx.core.service.ActiveInterviewService;
 import org.obiba.onyx.engine.Action;
 import org.obiba.onyx.engine.Stage;
@@ -23,33 +25,40 @@ public class StagePage extends BasePage {
   public StagePage(Stage stage) {
     super();
 
-    IStageExecution exec = activeInterviewService.getStageExecution(stage);
+    Participant participant = activeInterviewService.getParticipant();
 
-    final ActionWindow modal;
-    add(modal = new ActionWindow("modal") {
-
-      @Override
-      public void onActionPerformed(AjaxRequestTarget target, Stage stage, Action action) {
-        IStageExecution exec = activeInterviewService.getStageExecution(stage);
-        if(!exec.isInteractive()) {
-          setResponsePage(InterviewPage.class);
-        } else {
-          setResponsePage(new StagePage(stage));
-        }
-      }
-
-    });
-
-    add(new ActionsPanel("action", stage, exec, modal));
-
-    if(!exec.isInteractive()) {
-      add(new EmptyPanel("stage-component"));
+    if(participant == null) {
+      setResponsePage(WebApplication.get().getHomePage());
     } else {
-      Component stageComponent = exec.getWidget("stage-component");
-      if(stageComponent instanceof IEngineComponentAware) {
-        ((IEngineComponentAware) stageComponent).setActionWindwon(modal);
+
+      IStageExecution exec = activeInterviewService.getStageExecution(stage);
+
+      final ActionWindow modal;
+      add(modal = new ActionWindow("modal") {
+
+        @Override
+        public void onActionPerformed(AjaxRequestTarget target, Stage stage, Action action) {
+          IStageExecution exec = activeInterviewService.getStageExecution(stage);
+          if(!exec.isInteractive()) {
+            setResponsePage(InterviewPage.class);
+          } else {
+            setResponsePage(new StagePage(stage));
+          }
+        }
+
+      });
+
+      add(new ActionsPanel("action", stage, exec, modal));
+
+      if(!exec.isInteractive()) {
+        add(new EmptyPanel("stage-component"));
+      } else {
+        Component stageComponent = exec.getWidget("stage-component");
+        if(stageComponent instanceof IEngineComponentAware) {
+          ((IEngineComponentAware) stageComponent).setActionWindwon(modal);
+        }
+        add(stageComponent);
       }
-      add(stageComponent);
     }
   }
 
