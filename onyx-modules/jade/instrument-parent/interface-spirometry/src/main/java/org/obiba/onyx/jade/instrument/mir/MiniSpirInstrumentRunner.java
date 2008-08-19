2b352790e61357e89270a64bf5f18a37d5678d9a
Alice Carey <alice.carey@mail.mcgill.ca>
2008-08-19 13:02:28 +0000

    JADE-22 Correction du code-review: mise en forme du code pour le spirometre JADE-19 Définition des entrées et sorties pour le spiromètre

--- onyx-modules/jade/instrument-parent/interface-spirometry/src/main/java/org/obiba/onyx/jade/instrument/mir/MiniSpirInstrumentRunner.java
+++ onyx-modules/jade/instrument-parent/interface-spirometry/src/main/java/org/obiba/onyx/jade/instrument/mir/MiniSpirInstrumentRunner.java
@@ -2,12 +2,14 @@ package org.obiba.onyx.jade.instrument.mir;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,6 +18,7 @@ import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 
+import org.apache.wicket.util.io.Streams;
 import org.obiba.core.util.FileUtil;
 import org.obiba.onyx.jade.client.JnlpClient;
 import org.obiba.onyx.jade.instrument.ExternalAppLauncherHelper;
@@ -29,15 +32,22 @@ import org.slf4j.LoggerFactory;
 public class MiniSpirInstrumentRunner implements InstrumentRunner {
 
   private static final Logger log = LoggerFactory.getLogger(JnlpClient.class);
-  
-	//Injected by spring.
+
+  // Injected by spring.
   protected InstrumentExecutionService instrumentExecutionService;
+
   protected ExternalAppLauncherHelper externalAppHelper;
+
   private String mirPath;
+
   private String initdbPath;
+
   private String externalDbName;
+
   private String externalInputName;
+
   private String externalOutputName;
+
   private String externalImageName;
 
   public InstrumentExecutionService getInstrumentExecutionService() {
@@ -63,7 +73,7 @@ public class MiniSpirInstrumentRunner implements InstrumentRunner {
   public void setMirPath(String mirPath) {
     this.mirPath = mirPath;
   }
-  
+
   public String getInitdbPath() {
     return initdbPath;
   }
@@ -71,7 +81,7 @@ public class MiniSpirInstrumentRunner implements InstrumentRunner {
   public void setInitdbPath(String initdbPath) {
     this.initdbPath = initdbPath;
   }
-  
+
   public String getExternalDbName() {
     return externalDbName;
   }
@@ -79,7 +89,7 @@ public class MiniSpirInstrumentRunner implements InstrumentRunner {
   public void setExternalDbName(String externalDbName) {
     this.externalDbName = externalDbName;
   }
-  
+
   public String getExternalInputName() {
     return externalInputName;
   }
@@ -87,160 +97,138 @@ public class MiniSpirInstrumentRunner implements InstrumentRunner {
   public void setExternalInputName(String externalInputName) {
     this.externalInputName = externalInputName;
   }
-  
-  public String getExternalOutputName() {
-  	return externalOutputName;
-  }
-  
-  public void setExternalOutputName(String externalOutputName) {
-  	this.externalOutputName = externalOutputName;
-  }
-  
-  public String getExternalImageName() {
-  	return externalImageName;
-  }
-  
-  public void setExternalImageName(String externalImageName) {
-  	this.externalImageName = externalImageName;
-  }
-  
-  public void initParticipantData() throws Exception{
-  	// normally should retrieve latest data from database with current participant
-  	
-  	// Write participant data in spirometer input file
-  	File externalAppInputFile = new File(getMirPath() + getExternalInputName());
-		try {
-			//Map<String, Data> inputData = instrumentExecutionService.getInputParametersValue("ID", "LastName");
-			
-			// Test: to be removed
-			Map<String, Data> inputData = new HashMap<String, Data>();
-			inputData.put("ID", new Data(DataType.INTEGER, Integer.valueOf("2")));
-			inputData.put("LastName", new Data(DataType.TEXT, "Dupond"));
-			inputData.put("FirstName", new Data(DataType.TEXT, "Émilie"));
-			inputData.put("Gender", new Data(DataType.BOOLEAN, Boolean.valueOf("true")));
-			inputData.put("Height", new Data(DataType.DECIMAL, Double.valueOf("169")));
-			inputData.put("Weight", new Data(DataType.DECIMAL, Double.valueOf("57")));
-			inputData.put("EthnicGroup", new Data(DataType.INTEGER, Integer.valueOf("1")));
-			inputData.put("BirthDate", new Data(DataType.TEXT, "04/09/1979"));
-			// End Test
-			
-			BufferedWriter inputFileWriter = new BufferedWriter(new FileWriter(externalAppInputFile));
-			inputFileWriter.write( "[Identification]\n" );
-			for(String keyStr : inputData.keySet()) {
-				inputFileWriter.write( keyStr + "=" + inputData.get(keyStr).getValue().toString() + "\n" );
-			}
-			inputFileWriter.close();
-		} catch ( Exception ex ) {
-			throw new Exception( "Error writing spirometer input file: ", ex );
-		}
-  }
-  
-  protected void deleteDeviceData() throws Exception {
-		File backupDbFile = new File(getInitdbPath() + getExternalDbName()); 
-		File currentDbFile = new File(getMirPath() + getExternalDbName()); 
-		
-		if ( backupDbFile.exists() ) {
-			FileUtil.copyFile(backupDbFile, currentDbFile);
-		} else {
-			new File(getInitdbPath()).mkdir();
-			FileUtil.copyFile( currentDbFile, backupDbFile );
-		}
-	}
-  
-  private LinkedHashMap<String, Double[]> retrieveDeviceData() throws Exception {
-		
-  	//INCMOPLETE: Should retrieve all data, unit and image?
-  	
-		InputStream resultFileStrm = null;
-		LinkedHashMap<String, Double[]> outputData = new LinkedHashMap<String, Double[]>();
-		try {
-			resultFileStrm = new FileInputStream(getMirPath() + getExternalOutputName());
-			BufferedReader fileReader = new BufferedReader( new InputStreamReader( resultFileStrm ) );
 
-			StringBuffer wResults = new StringBuffer();
-			String line;
-			Boolean lastParam = false;
-			while ( (line = fileReader.readLine()) != null ) {
-				wResults.append( line + "\n" );
-				if (line.indexOf("PIF") == 0)
-					lastParam = true;
-			}
-			if (lastParam == false)
-				JOptionPane.showMessageDialog(null, "Data is incomplete", "Could not complete process", JOptionPane.ERROR_MESSAGE);
-			
-			Pattern pattern = Pattern.compile( "(.*)\t(.*)\t(.*)\t(.*)\t(.*)" );
-			Matcher matcher = pattern.matcher( wResults.toString().replace( ",", "." ).replace( "/", "_" ) );
-			String description = null;
-			Double[] data = null;
-			
-			while(matcher.find()) {
-				description = matcher.group(1);
-				data = new Double[2];
-				data[0] = Double.valueOf( matcher.group(3) );
-				data[1] = Double.valueOf( matcher.group(4) );
-				outputData.put( description, data );
-			}
-			
-			try {
-				resultFileStrm.close();
-			} catch ( Exception ex ) {
-				log.info("*** Error in closing spirometry output data file stream: " + ex.getStackTrace());
-			}			
-						
-		} catch ( FileNotFoundException fnfEx ) {
-			log.info("*** Error: spirometry output data file not found: " + fnfEx.getStackTrace());
-			JOptionPane.showMessageDialog(null, "Error: spirometry output data file not found", "Could not complete process", JOptionPane.ERROR_MESSAGE);
-		}
-		return outputData;
-	}
+  public String getExternalOutputName() {
+    return externalOutputName;
+  }
+
+  public void setExternalOutputName(String externalOutputName) {
+    this.externalOutputName = externalOutputName;
+  }
+
+  public String getExternalImageName() {
+    return externalImageName;
+  }
+
+  public void setExternalImageName(String externalImageName) {
+    this.externalImageName = externalImageName;
+  }
+
+  public void initParticipantData() throws Exception {
+    // normally should retrieve latest data from database with current participant
+
+    // Write participant data in spirometer input file
+    File externalAppInputFile = new File(getMirPath() + getExternalInputName());
+    try {
+      Map<String, Data> inputData = instrumentExecutionService.getInputParametersValue("ID", "LastName", "FirstName", "Gender", "Height", "Weight", "EthnicGroup", "BirthDate");
+
+      BufferedWriter inputFileWriter = new BufferedWriter(new FileWriter(externalAppInputFile));
+      inputFileWriter.write("[Identification]\n");
+      for(String keyStr : inputData.keySet()) {
+        inputFileWriter.write(keyStr + "=" + inputData.get(keyStr).getValue().toString() + "\n");
+      }
+      inputFileWriter.close();
+    } catch(Exception ex) {
+      throw new Exception("Error writing spirometer input file: ", ex);
+    }
+  }
+
+  protected void deleteDeviceData() throws Exception {
+    File backupDbFile = new File(getInitdbPath() + getExternalDbName());
+    File currentDbFile = new File(getMirPath() + getExternalDbName());
+
+    if(backupDbFile.exists()) {
+      FileUtil.copyFile(backupDbFile, currentDbFile);
+    } else {
+      new File(getInitdbPath()).mkdir();
+      FileUtil.copyFile(currentDbFile, backupDbFile);
+    }
+  }
+
+  private LinkedHashMap<String, Double[]> retrieveDeviceData() throws Exception {
+
+    InputStream resultFileStrm = null;
+    LinkedHashMap<String, Double[]> outputData = new LinkedHashMap<String, Double[]>();
+    try {
+      resultFileStrm = new FileInputStream(getMirPath() + getExternalOutputName());
+      BufferedReader fileReader = new BufferedReader(new InputStreamReader(resultFileStrm));
+
+      StringBuffer wResults = new StringBuffer();
+      String line;
+      Boolean lastParam = false;
+      while((line = fileReader.readLine()) != null) {
+        wResults.append(line + "\n");
+        if(line.indexOf("PIF") == 0) lastParam = true;
+      }
+      if(lastParam == false) JOptionPane.showMessageDialog(null, "Data is incomplete", "Could not complete process", JOptionPane.ERROR_MESSAGE);
+
+      Pattern pattern = Pattern.compile("(.*)\t(.*)\t(.*)\t(.*)\t(.*)");
+      Matcher matcher = pattern.matcher(wResults.toString().replace(",", ".").replace("/", "_"));
+      String description = null;
+      Double[] data = null;
+
+      while(matcher.find()) {
+        description = matcher.group(1);
+        data = new Double[2];
+        data[0] = Double.valueOf(matcher.group(3));
+        data[1] = Double.valueOf(matcher.group(4));
+        outputData.put(description, data);
+      }
+
+      try {
+        resultFileStrm.close();
+      } catch(Exception ex) {
+        log.info("*** Error in closing spirometry output data file stream: " + ex.getStackTrace());
+      }
+
+    } catch(FileNotFoundException fnfEx) {
+      log.info("*** Error: spirometry output data file not found: " + fnfEx.getStackTrace());
+      JOptionPane.showMessageDialog(null, "Error: spirometry output data file not found", "Could not complete process", JOptionPane.ERROR_MESSAGE);
+    }
+    return outputData;
+  }
 
   public void SendDataToServer(LinkedHashMap<String, Double[]> results) throws Exception {
-  	Map<String, Data> ouputToSend = new HashMap<String, Data>();
-  	
-  	for (String keyStr : results.keySet()) {
-  		Double[] valueArray = results.get(keyStr);
-  		ouputToSend.put(keyStr, new Data(DataType.DECIMAL, valueArray[0]));
-  		ouputToSend.put(keyStr + "_pred", new Data(DataType.DECIMAL, valueArray[1]));
-  	}
-  	
-  	// Should we keep FVC Image???
-  	/*File FVCFile = new File( getMirPath() + getExternalImageName() );
-		FileInputStream FVCInputStream = new FileInputStream( FVCFile );
-		ouputToSend.put("FVCImage", new Data(DataType.DATA, FVCInputStream));*/
-		
+    Map<String, Data> ouputToSend = new HashMap<String, Data>();
+
+    for(String keyStr : results.keySet()) {
+      Double[] valueArray = results.get(keyStr);
+      ouputToSend.put(keyStr, new Data(DataType.DECIMAL, valueArray[0]));
+      ouputToSend.put(keyStr + "_pred", new Data(DataType.DECIMAL, valueArray[1]));
+    }
+
+    // Save the FVC image
+    File FVCFile = new File(getMirPath() + getExternalImageName());
+    String fileContent = Streams.readString(new FileInputStream(FVCFile), "UTF-8");
+    ByteArrayInputStream FVCInputStream = new ByteArrayInputStream(fileContent.getBytes("UTF-8"));
+    ouputToSend.put("FVCImage", new Data(DataType.DATA, (Serializable) FVCInputStream));
+
     instrumentExecutionService.addOutputParameterValues(ouputToSend);
   }
-  
+
   public void initialize() {
-  	log.info( "*** Initializing MIR Runner ***" );
-  	try {
-  		deleteDeviceData();				// Delete ancient data in instrument specific database
-  		initParticipantData();		// Create file with participant data
-  	} catch ( Exception ex ) {
-  		log.info("*** EXCEPTION INITIALIZE STEP: " + ex.getStackTrace());
-		}
+    log.info("*** Initializing MIR Runner ***");
+    try {
+      deleteDeviceData(); // Delete ancient data in instrument specific database
+      initParticipantData(); // Create file with participant data
+    } catch(Exception ex) {
+      log.info("*** EXCEPTION INITIALIZE STEP: " + ex.getStackTrace());
+    }
   }
 
   public void run() {
-  	log.info( "*** Running MIR Runner ***" );    
+    log.info("*** Running MIR Runner ***");
     externalAppHelper.launch();
   }
 
   public void shutdown() {
-  	log.info( "*** Shutdown MIR Runner ***" );
-  	// Get data from external app
-  	try {
-  		LinkedHashMap<String, Double[]> results = retrieveDeviceData();
-  		SendDataToServer(results);
-  	} catch( Exception ex) {
-  		log.info("*** EXCEPTION SHUTDOWN STEP: " + ex.getStackTrace());
-  	}
-  	
-  	// Send data to server => How ???
+    log.info("*** Shutdown MIR Runner ***");
+    // Get data from external app
+    try {
+      LinkedHashMap<String, Double[]> results = retrieveDeviceData();
+      SendDataToServer(results);
+    } catch(Exception ex) {
+      log.info("*** EXCEPTION SHUTDOWN STEP: " + ex.getStackTrace());
+    }
   }
-  
 }
-
-
-
-
