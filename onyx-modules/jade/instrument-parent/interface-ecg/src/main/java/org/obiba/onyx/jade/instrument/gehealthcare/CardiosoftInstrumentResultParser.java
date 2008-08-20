57903be354d50f854792b9775184db9f0d72ad57
Alice Carey <alice.carey@mail.mcgill.ca>
2008-08-20 18:16:09 +0000

    JADE-17 Implémentation des classes CardiosoftInstrumentRunner qui procède à l'execution de l'application externe pour l'ecg et CardiosoftInstrumentResultParser qui parse le fichier xml résultant d'une mesure d'ecg Définition des paramètres d'entrée et sortie, de l'instrument-context Inclusion dans le directory install du fichier de modification de base de registre et autres fichiers nécessaires à la configuration de l'application externe (cardio.ini et codesecg.txt)

---
+++ onyx-modules/jade/instrument-parent/interface-ecg/src/main/java/org/obiba/onyx/jade/instrument/gehealthcare/CardiosoftInstrumentResultParser.java
@@ -0,0 +1,324 @@
+package org.obiba.onyx.jade.instrument.gehealthcare;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class CardiosoftInstrumentResultParser {
+
+  // The whole XML document
+  private String xmlDocument;
+
+  private String time;
+
+  private String date;
+
+  private String diagnosis;
+
+  private String conclusion;
+
+  private String filterSetting;
+
+  // Resting ECG Measurements
+  private String diagnosisVersion;
+
+  private Long ventricularRate;
+
+  private Long pQInterval;
+
+  private Long pDuration;
+
+  private Long qRsDuration;
+
+  private Long qTInterval;
+
+  private Long qTCInterval;
+
+  private Long rRInterval;
+
+  private Long pPInterval;
+
+  private Long sokolovLVHIndex;
+
+  private Long pAxis;
+
+  private Long rAxis;
+
+  private Long tAxis;
+
+  private Long qTDispersion;
+
+  private Long qTDispersionBazett;
+
+  private Long qRSNum;
+
+  private Long pOnset;
+
+  private Long pOffset;
+
+  private Long qOnset;
+
+  private Long qOffset;
+
+  private Long tOffset;
+
+  private XPath xpath;
+
+  private Document doc;
+
+  @SuppressWarnings("deprecation")
+  public CardiosoftInstrumentResultParser(InputStream pFileStream) throws IOException {
+    try {
+      // First read the whole file to keep a copy in the EcgCollector object.
+      BufferedReader fileReader = new BufferedReader(new InputStreamReader(pFileStream));
+      StringBuilder xmlFileContent = new StringBuilder();
+      String wOneLine;
+      int i = 0;
+      while((wOneLine = fileReader.readLine()) != null) {
+        xmlFileContent.append(wOneLine + "\n");
+        i++;
+      }
+      xmlDocument = xmlFileContent.toString();
+
+      // Now create a stream using the copy of the XML file for the XPath analysis
+      InputStream xmlStream = new java.io.StringBufferInputStream(xmlDocument);
+      DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+      domFactory.setNamespaceAware(true);
+      DocumentBuilder builder = domFactory.newDocumentBuilder();
+      doc = builder.parse(xmlStream);
+
+      XPathFactory factory = XPathFactory.newInstance();
+      xpath = factory.newXPath();
+
+      extractDateTime();
+      extractInterpretation();
+      extractFilterSetting();
+      extractRestingEcgMeasurements();
+    } catch(XPathExpressionException e) {
+      System.out.println("Invalid XPath expression. Stopping XML data extraction.");
+    } catch(ParserConfigurationException e) {
+      System.out.println("An error has occured while trying to initialize the XPath parser.");
+    } catch(SAXException e) {
+      System.out.println("An error has occured while trying to initialize the XPath parser.");
+    }
+  }
+
+  public String getXmlDocument() {
+    return xmlDocument;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public String getDiagnosis() {
+    return diagnosis;
+  }
+
+  public String getConclusion() {
+    return conclusion;
+  }
+
+  public String getFilterSetting() {
+    return filterSetting;
+  }
+
+  public String getDiagnosisVersion() {
+    return diagnosisVersion;
+  }
+
+  public Long getVentricularRate() {
+    return ventricularRate;
+  }
+
+  public Long getPQInterval() {
+    return pQInterval;
+  }
+
+  public Long getPDuration() {
+    return pDuration;
+  }
+
+  public Long getQRsDuration() {
+    return qRsDuration;
+  }
+
+  public Long getQTInterval() {
+    return qTInterval;
+  }
+
+  public Long getQTCInterval() {
+    return qTCInterval;
+  }
+
+  public Long getRRInterval() {
+    return rRInterval;
+  }
+
+  public Long getPPInterval() {
+    return pPInterval;
+  }
+
+  public Long getSokolovLVHIndex() {
+    return sokolovLVHIndex;
+  }
+
+  public Long getPAxis() {
+    return pAxis;
+  }
+
+  public Long getRAxis() {
+    return rAxis;
+  }
+
+  public Long getTAxis() {
+    return tAxis;
+  }
+
+  public Long getQTDispersion() {
+    return qTDispersion;
+  }
+
+  public Long getQTDispersionBazett() {
+    return qTDispersionBazett;
+  }
+
+  public Long getQRSNum() {
+    return qRSNum;
+  }
+
+  public Long getPOnset() {
+    return pOnset;
+  }
+
+  public Long getPOffset() {
+    return pOffset;
+  }
+
+  public Long getQOnset() {
+    return qOnset;
+  }
+
+  public Long getQOffset() {
+    return qOffset;
+  }
+
+  public Long getTOffset() {
+    return tOffset;
+  }
+
+  /**
+   * Extracts date and time of measurement.
+   * @throws XPathExpressionException
+   */
+  private void extractDateTime() throws XPathExpressionException {
+
+    time = xpath.evaluate("concat(//ObservationDateTime/Hour/text(), ':', //ObservationDateTime/Minute/text(), ':', //ObservationDateTime/Second/text())", doc, XPathConstants.STRING).toString();
+
+    date = xpath.evaluate("concat(//ObservationDateTime/Year/text(), '-', //ObservationDateTime/Month/text(), '-', //ObservationDateTime/Day/text())", doc, XPathConstants.STRING).toString();
+  }
+
+  /**
+   * Extracts software's data interpretation.
+   * @throws XPathExpressionException
+   */
+  private void extractInterpretation() throws XPathExpressionException {
+
+    // Extracting Diagnosis
+    Object diagnosisNode = xpath.evaluate("//Interpretation/Diagnosis/DiagnosisText/text()", doc, XPathConstants.NODESET);
+    StringBuilder diagnosisSb = new StringBuilder();
+    NodeList nodes = (NodeList) diagnosisNode;
+    for(int i = 0; i < nodes.getLength(); i++) {
+      diagnosisSb.append(nodes.item(i).getNodeValue());
+      if(i + 1 < nodes.getLength()) {
+        diagnosisSb.append("\n");
+      }
+    }
+    diagnosis = diagnosisSb.toString();
+
+    // Extracting Conclusion
+    Object conclusionNode = xpath.evaluate("//Interpretation/Conclusion/ConclusionText/text()", doc, XPathConstants.NODESET);
+    StringBuilder conclusionSb = new StringBuilder();
+    nodes = (NodeList) conclusionNode;
+    for(int i = 0; i < nodes.getLength(); i++) {
+      conclusionSb.append(nodes.item(i).getNodeValue());
+      if(i + 1 < nodes.getLength()) {
+        conclusionSb.append("\n");
+      }
+    }
+    conclusion = conclusionSb.toString();
+  }
+
+  /**
+   * Extracts filter data.
+   */
+  private void extractFilterSetting() throws XPathExpressionException {
+    filterSetting = xpath.evaluate("concat('Cublic Spline: ', //FilterSetting/CubicSpline/text(), " + "     '\nFilter50Hz: ',    //FilterSetting/Filter50Hz/text(),  " + "     '\nFilter60Hz: ',      //FilterSetting/Filter60Hz/text(),  " + "     '\nLowPass (', //FilterSetting/LowPass/attribute::units, '): ',    //FilterSetting/LowPass/text(),     " + "     '\nHighPass (', //FilterSetting/HighPass/attribute::units, '): ',    //FilterSetting/HighPass/text())", doc, XPathConstants.STRING).toString();
+  }
+
+  /**
+   * Extract all data located under the RestingEcgMeasurements tag.
+   * @throws XPathExpressionException
+   */
+  private void extractRestingEcgMeasurements() throws XPathExpressionException {
+    diagnosisVersion = xpath.evaluate("//RestingECGMeasurements/DiagnosisVersion/text()", doc, XPathConstants.STRING).toString();
+    ventricularRate = extractMeasurement("VentricularRate");
+    pQInterval = extractMeasurement("PQInterval");
+    pDuration = extractMeasurement("PDuration");
+    qRsDuration = extractMeasurement("QRSDuration");
+    qTInterval = extractMeasurement("QTInterval");
+    qTCInterval = extractMeasurement("QTCInterval");
+    rRInterval = extractMeasurement("RRInterval");
+    pPInterval = extractMeasurement("PPInterval");
+    sokolovLVHIndex = extractMeasurement("SokolovLVHIndex");
+    pAxis = extractMeasurement("PAxis");
+    rAxis = extractMeasurement("RAxis");
+    tAxis = extractMeasurement("TAxis");
+    qTDispersion = extractMeasurement("QTDispersion");
+    qTDispersionBazett = extractMeasurement("QTDispersionBazett");
+    qRSNum = Long.valueOf(xpath.evaluate("//RestingECGMeasurements/QRSNum/text()", doc, XPathConstants.STRING).toString());
+    pOnset = extractMeasurement("POnset");
+    pOffset = extractMeasurement("POffset");
+    qOnset = extractMeasurement("QOnset");
+    qOffset = extractMeasurement("QOffset");
+    tOffset = extractMeasurement("TOffset");
+  }
+
+  /*private String extractMeasurementAndUnit(String tagName) throws XPathExpressionException {
+    String measurement = xpath.evaluate("//RestingECGMeasurements/" + tagName + "/text()", doc, XPathConstants.STRING).toString();
+    String measurementUnit = xpath.evaluate("//RestingECGMeasurements/" + tagName + "/attribute::units", doc, XPathConstants.STRING).toString();
+
+    if(measurement.equals("")) {
+      return "None";
+    } else {
+      return measurement + ' ' + measurementUnit;
+    }
+  }*/
+  
+  private Long extractMeasurement(String tagName) throws XPathExpressionException {
+    String measurement = xpath.evaluate("//RestingECGMeasurements/" + tagName + "/text()", doc, XPathConstants.STRING).toString();
+    
+    if(measurement.equals("")) {
+      return null;
+    } else {
+      return Long.valueOf(measurement);
+    }
+  }
+}
--- onyx-modules/jade/instrument-parent/interface-ecg/src/main/java/org/obiba/onyx/jade/instrument/gehealthcare/CardiosoftInstrumentRunner.java
+++ onyx-modules/jade/instrument-parent/interface-ecg/src/main/java/org/obiba/onyx/jade/instrument/gehealthcare/CardiosoftInstrumentRunner.java
@@ -1,16 +1,59 @@
 package org.obiba.onyx.jade.instrument.gehealthcare;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import org.apache.wicket.util.io.Streams;
+import org.obiba.core.util.FileUtil;
+import org.obiba.onyx.core.domain.participant.Gender;
+import org.obiba.onyx.core.domain.participant.Participant;
+import org.obiba.onyx.jade.client.JnlpClient;
 import org.obiba.onyx.jade.instrument.ExternalAppLauncherHelper;
 import org.obiba.onyx.jade.instrument.InstrumentRunner;
 import org.obiba.onyx.jade.instrument.service.InstrumentExecutionService;
+import org.obiba.onyx.util.data.Data;
+import org.obiba.onyx.util.data.DataType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CardiosoftInstrumentRunner implements InstrumentRunner {
+
+  private static final Logger log = LoggerFactory.getLogger(JnlpClient.class);
 
   // Injected by spring.
   protected InstrumentExecutionService instrumentExecutionService;
 
-  // Injected by spring.
-  ExternalAppLauncherHelper externalAppHelper;
+  protected ExternalAppLauncherHelper externalAppHelper;
+
+  private String cardioPath;
+
+  private String initPath;
+
+  private String databasePath;
+
+  private String exportPath;
+
+  private String settingsFileName;
+
+  private String xmlFileName;
+
+  private String pdfFileName;
 
   public InstrumentExecutionService getInstrumentExecutionService() {
     return instrumentExecutionService;
@@ -28,17 +71,153 @@ public class CardiosoftInstrumentRunner implements InstrumentRunner {
     this.externalAppHelper = externalAppHelper;
   }
 
+  public String getCardioPath() {
+    return cardioPath;
+  }
+
+  public void setCardioPath(String cardioPath) {
+    this.cardioPath = cardioPath;
+  }
+
+  public String getInitPath() {
+    return initPath;
+  }
+
+  public void setInitPath(String initPath) {
+    this.initPath = initPath;
+  }
+
+  public String getDatabasePath() {
+    return databasePath;
+  }
+
+  public void setDatabasePath(String databasePath) {
+    this.databasePath = databasePath;
+  }
+
+  public String getExportPath() {
+    return exportPath;
+  }
+
+  public void setExportPath(String exportPath) {
+    this.exportPath = exportPath;
+  }
+
+  public String getSettingsFileName() {
+    return settingsFileName;
+  }
+
+  public void setSettingsFileName(String settingsFileName) {
+    this.settingsFileName = settingsFileName;
+  }
+
+  public String getXmlFileName() {
+    return xmlFileName;
+  }
+
+  public void setXmlFileName(String xmlFileName) {
+    this.xmlFileName = xmlFileName;
+  }
+
+  public String getPdfFileName() {
+    return pdfFileName;
+  }
+
+  public void setPdfFileName(String pdfFileName) {
+    this.pdfFileName = pdfFileName;
+  }
+
+  protected void deleteDeviceData() throws Exception {
+    // Overwrite the CardioSoft configuration file
+    File backupSettingsFile = new File(getInitPath() + getSettingsFileName());
+    File currentSettingsFile = new File(getCardioPath() + getSettingsFileName());
+    if(backupSettingsFile.exists() && !(backupSettingsFile.lastModified() == currentSettingsFile.lastModified())) {
+      FileUtil.copyFile(backupSettingsFile, currentSettingsFile);
+      FileUtil.copyFile(currentSettingsFile, backupSettingsFile);// to set same lastModified property
+    }
+
+    // Delete the Pervasive database files
+    FilenameFilter filter = new FilenameFilter() {
+      public boolean accept(File dir, String name) {
+        return (name.endsWith(".BTR"));
+      }
+    };
+    File[] databaseFiles = new File(getDatabasePath()).listFiles(filter);
+    for(int i = 0; i < databaseFiles.length; i++) {
+      databaseFiles[i].delete();
+    }
+
+    // Delete generated reports
+    File reportFile = new File(getExportPath() + getXmlFileName());
+    reportFile.delete();
+    reportFile = new File(getExportPath() + getPdfFileName());
+    reportFile.delete();
+  }
+
+  public void SendDataToServer(CardiosoftInstrumentResultParser resultParser) throws Exception {
+    Map<String, Data> ouputToSend = new HashMap<String, Data>();
+
+    Class resultParserClass = CardiosoftInstrumentResultParser.class;
+    Method method;
+    try {
+      Field[] resultParserFields = resultParserClass.getDeclaredFields();
+      for(Field field : resultParserFields) {
+        if(field.getName().equalsIgnoreCase("doc") || field.getName().equalsIgnoreCase("xpath") || field.getName().equalsIgnoreCase("xmldocument")) continue;
+        method = resultParserClass.getDeclaredMethod("get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1));
+        Object value = method.invoke(resultParser);
+        if(value instanceof Long) ouputToSend.put(field.getName(), new Data(DataType.INTEGER, (Serializable) value));
+        else
+          ouputToSend.put(field.getName(), new Data(DataType.TEXT, (Serializable) value));
+      }
+
+      // Save the xml and pdf files
+      File xmlFile = new File(getExportPath() + getXmlFileName());
+      String fileContent = Streams.readString(new FileInputStream(xmlFile), "UTF-8");
+      ByteArrayInputStream xmlInputStream = new ByteArrayInputStream(fileContent.getBytes("UTF-8"));
+      ouputToSend.put("xmlFile", new Data(DataType.DATA, (Serializable) xmlInputStream));
+
+      File pdfFile = new File(getExportPath() + getPdfFileName());
+      fileContent = Streams.readString(new FileInputStream(pdfFile), "UTF-8");
+      ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(fileContent.getBytes("UTF-8"));
+      ouputToSend.put("pdfFile", new Data(DataType.DATA, (Serializable) pdfInputStream));
+
+      instrumentExecutionService.addOutputParameterValues(ouputToSend);
+
+    } catch(FileNotFoundException fnfEx) {
+      log.info("*** Error: Cardiosoft output data file not found: " + fnfEx.getStackTrace());
+      JOptionPane.showMessageDialog(null, "Error: Cardiosoft output data file not found", "Could not complete process", JOptionPane.ERROR_MESSAGE);
+    } catch(Exception e) {
+      log.warn("Failed result: " + e.getMessage(), e);
+    }
+  }
+
   public void initialize() {
-    System.out.println( "*** Initializing Cardiosoft Runner ***" );
+    log.info("*** Initializing Cardiosoft Runner ***");
+    try {
+      deleteDeviceData(); // Delete ancient data in instrument specific database
+    } catch(Exception ex) {
+      log.info("*** EXCEPTION INITIALIZE STEP: " + ex.getStackTrace());
+    }
   }
 
   public void run() {
-    System.out.println( "*** Running Cardiosoft Runner ***" );    
+    log.info("*** Running Cardiosoft Runner ***");
     externalAppHelper.launch();
   }
 
   public void shutdown() {
-    System.out.println( "*** Shutdown Cardiosoft Runner ***" );
+    log.info("*** Shutdown Cardiosoft Runner ***");
+    // Get data from external app
+    try {
+      FileInputStream resultInputStream = new FileInputStream(getExportPath() + getXmlFileName());
+      CardiosoftInstrumentResultParser resultParser = new CardiosoftInstrumentResultParser(resultInputStream);
+      SendDataToServer(resultParser);
+    } catch(FileNotFoundException fnfEx) {
+      log.info("*** Error: Cardiosoft output data file not found: " + fnfEx.getStackTrace());
+      JOptionPane.showMessageDialog(null, "Error: Cardiosoft output data file not found", "Could not complete process", JOptionPane.ERROR_MESSAGE);
+    } catch(Exception ex) {
+      log.info("*** EXCEPTION SHUTDOWN STEP: " + ex.getStackTrace());
+    }
   }
 
 }
