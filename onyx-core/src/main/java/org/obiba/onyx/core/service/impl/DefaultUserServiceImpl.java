4e04c5e680d0538d1d4ddb8fb79b435cd08c3216
Alice Carey <alice.carey@mail.mcgill.ca>
2008-09-11 19:26:00 +0000

    bug fixes dans fonction isNewPassword

--- onyx-core/src/main/java/org/obiba/onyx/core/service/impl/DefaultUserServiceImpl.java
+++ onyx-core/src/main/java/org/obiba/onyx/core/service/impl/DefaultUserServiceImpl.java
@@ -3,7 +3,6 @@ package org.obiba.onyx.core.service.impl;
 import java.util.Locale;
 
 import org.obiba.core.service.impl.PersistenceManagerAwareService;
-import org.obiba.onyx.core.domain.user.Role;
 import org.obiba.onyx.core.domain.user.Status;
 import org.obiba.onyx.core.domain.user.User;
 import org.obiba.onyx.core.service.UserService;
@@ -21,56 +20,59 @@ public abstract class DefaultUserServiceImpl extends PersistenceManagerAwareServ
     template.setLogin(login);
     return getPersistenceManager().matchOne(template);
   }
-  
+
   public void changeStatus(User user, Status status) {
     User userToChange = getPersistenceManager().matchOne(user);
     userToChange.setStatus(status);
     getPersistenceManager().save(userToChange);
   }
-  
+
   public void createUser(User user) {
     getPersistenceManager().save(user);
   }
-  
+
   public void deleteUser(User user) {
     User userToDelete = getPersistenceManager().matchOne(user);
     userToDelete.setDeleted(true);
     getPersistenceManager().save(userToDelete);
   }
-  
+
   public void setUserLanguage(User template, Locale language) {
     User user = getPersistenceManager().matchOne(template);
     user.setLanguage(language);
     getPersistenceManager().save(user);
   }
-  
+
   public boolean isNewPassword(User user, String password) {
-    user.setPassword(password);
-    return (getPersistenceManager().matchOne(user) == null);
+    User template = new User();
+    template.setLogin(user.getLogin());
+    template.setPassword(password);
+
+    return (getPersistenceManager().matchOne(template) == null);
   }
-  
+
   public void setPassword(User template, String password) {
     User user = getPersistenceManager().matchOne(template);
     user.setPassword(password);
     getPersistenceManager().save(user);
   }
-  
-  public void setUser (User user) {
+
+  public void setUser(User user) {
     User template = new User();
-    if (user.getId() != null) {
+    if(user.getId() != null) {
       template.setLogin(user.getLogin());
       template = getPersistenceManager().matchOne(template);
-      
+
       if(!user.getLastName().equals(template.getLastName())) template.setLastName(user.getLastName());
       if(!user.getFirstName().equals(template.getFirstName())) template.setFirstName(user.getFirstName());
       if(!user.getPassword().equals(template.getPassword())) template.setPassword(user.getPassword());
       if(!user.getEmail().equals(template.getEmail())) template.setEmail(user.getEmail());
-      if(!user.getLanguage().equals(template.getLanguage())) template.setLanguage(user.getLanguage());
-      
+      if(template.getLanguage() == null || !user.getLanguage().equals(template.getLanguage())) template.setLanguage(user.getLanguage());
+
       if(!user.getRoles().equals(template.getRoles())) {
         template.setRoles(user.getRoles());
       }
-      
+
     } else {
       template = user;
     }
