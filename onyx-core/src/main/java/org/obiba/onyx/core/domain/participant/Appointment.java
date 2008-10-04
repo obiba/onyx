b0221dfe72e75407f40f3a8b361cb4c4cf5a9158
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-10-04 04:07:27 +0000

    Organized imports

--- onyx-core/src/main/java/org/obiba/onyx/core/domain/participant/Appointment.java
+++ onyx-core/src/main/java/org/obiba/onyx/core/domain/participant/Appointment.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
