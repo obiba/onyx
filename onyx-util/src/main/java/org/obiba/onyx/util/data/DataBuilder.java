873a0745565bd9ecb5b4fbc90941bc6db6e73bf2
Yannick Marcon <yannick.marcon@obiba.org>
2008-10-06 17:25:01 +0000

    Added a localization wicket model using Spring. Added the possibility of building a Data given a DataType and the string representation of the value.

--- onyx-util/src/main/java/org/obiba/onyx/util/data/DataBuilder.java
+++ onyx-util/src/main/java/org/obiba/onyx/util/data/DataBuilder.java
@@ -6,45 +6,83 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DataBuilder {
-  
-  static Logger log = LoggerFactory.getLogger(DataBuilder.class);
+
+  private static final Logger log = LoggerFactory.getLogger(DataBuilder.class);
 
   public static Data buildBoolean(Boolean booleanValue) {
-    return new Data(DataType.BOOLEAN,booleanValue);
+    return new Data(DataType.BOOLEAN, booleanValue);
   }
-  
+
+  public static Data buildBoolean(String booleanValue) {
+    return new Data(DataType.BOOLEAN, Boolean.parseBoolean(booleanValue));
+  }
+
   public static Data buildDate(Date date) {
-    return new Data(DataType.DATE,date);
-  }  
-  
+    return new Data(DataType.DATE, date);
+  }
+
+  public static Data buildDate(String date) {
+    try {
+      return new Data(DataType.DATE, SimpleDateFormat.getInstance().parse(date));
+    } catch(ParseException e) {
+      throw new IllegalArgumentException("Unable to parse date string: " + date, e);
+    }
+  }
+
   public static Data buildDecimal(Double doubleValue) {
-    return new Data(DataType.DECIMAL,doubleValue);
-  }   
-  
+    return new Data(DataType.DECIMAL, doubleValue);
+  }
+
+  public static Data buildDecimal(String doubleValue) {
+    return new Data(DataType.DECIMAL, Double.parseDouble(doubleValue));
+  }
+
   public static Data buildDecimal(Float floatValue) {
-    return new Data(DataType.DECIMAL,floatValue);
-  }  
-  
+    return new Data(DataType.DECIMAL, floatValue);
+  }
+
   public static Data buildInteger(Long longValue) {
-    return new Data(DataType.INTEGER,longValue);
-  }   
-  
+    return new Data(DataType.INTEGER, longValue);
+  }
+
+  public static Data buildInteger(String longValue) {
+    return new Data(DataType.INTEGER, Long.parseLong(longValue));
+  }
+
   public static Data buildInteger(Integer integerValue) {
-    return new Data(DataType.INTEGER,integerValue);
-  }    
-  
+    return new Data(DataType.INTEGER, integerValue);
+  }
+
   public static Data buildText(String text) {
-    return new Data(DataType.TEXT,text);
-  }     
+    return new Data(DataType.TEXT, text);
+  }
+
+  public static Data build(DataType type, String value) {
+    switch(type) {
+    case BOOLEAN:
+      return buildBoolean(value);
+    case DECIMAL:
+      return buildDecimal(value);
+    case INTEGER:
+      return buildInteger(value);
+    case TEXT:
+      return buildText(value);
+    case DATE:
+      return buildDate(value);
+    }
+    return null;
+  }
 
   public static Data buildBinary(InputStream inputStream) {
-    
+
     ByteArrayOutputStream convertedStream = new ByteArrayOutputStream();
     byte[] readBuffer = new byte[1024];
     int bytesRead;
@@ -57,12 +95,12 @@ public class DataBuilder {
       log.error("Error while reading binary data stream", couldNotReadStream);
       throw new RuntimeException(couldNotReadStream);
     }
-    
-    return new Data(DataType.DATA,convertedStream.toByteArray());
+
+    return new Data(DataType.DATA, convertedStream.toByteArray());
   }
-  
+
   public static Data buildBinary(File file) {
-    
+
     FileInputStream inputStream;
     try {
       inputStream = new FileInputStream(file);
@@ -71,8 +109,7 @@ public class DataBuilder {
       throw new RuntimeException(fileNotFound);
     }
     return buildBinary(inputStream);
-    
-  }
 
+  }
 
 }
