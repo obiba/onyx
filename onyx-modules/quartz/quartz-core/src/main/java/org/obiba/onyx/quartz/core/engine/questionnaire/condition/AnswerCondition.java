35b819c341ec60c4504770944cd87ecb7c022fcb
Yannick Marcon <yannick.marcon@obiba.org>
2008-10-17 15:02:43 +0000

    Answer condition is linked to QuestionCategory.

--- onyx-modules/quartz/quartz-core/src/main/java/org/obiba/onyx/quartz/core/engine/questionnaire/condition/AnswerCondition.java
+++ onyx-modules/quartz/quartz-core/src/main/java/org/obiba/onyx/quartz/core/engine/questionnaire/condition/AnswerCondition.java
@@ -1,35 +1,37 @@
-/*******************************************************************************
+/***********************************************************************************************************************
  * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
  * 
- * This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0.
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ **********************************************************************************************************************/
 package org.obiba.onyx.quartz.core.engine.questionnaire.condition;
 
 import org.obiba.onyx.quartz.core.engine.questionnaire.question.Category;
 import org.obiba.onyx.quartz.core.engine.questionnaire.question.Question;
+import org.obiba.onyx.quartz.core.engine.questionnaire.question.QuestionCategory;
 
 public class AnswerCondition extends Condition {
 
   private static final long serialVersionUID = -7608048954030186313L;
 
-  private Question question;
+  private QuestionCategory questionCategory;
 
   private Integer occurence;
 
-  private Category category;
-
   private AnswerCondition parentAnswerCondition;
 
-  public Question getQuestion() {
-    return question;
+  public QuestionCategory getQuestionCategory() {
+    return questionCategory;
   }
 
-  public void setQuestion(Question question) {
-    this.question = question;
+  public void setQuestionCategory(QuestionCategory questionCategory) {
+    this.questionCategory = questionCategory;
+  }
+
+  public Question getQuestion() {
+    return questionCategory.getQuestion();
   }
 
   public Integer getOccurence() {
@@ -41,11 +43,7 @@ public class AnswerCondition extends Condition {
   }
 
   public Category getCategory() {
-    return category;
-  }
-
-  public void setCategory(Category category) {
-    this.category = category;
+    return questionCategory.getCategory();
   }
 
   public AnswerCondition getParentAnswerCondition() {
