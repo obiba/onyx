d04d914b2b5cfff54dcae98381acd1da4ba7219e
Yannick Marcon <yannick.marcon@obiba.org>
2008-10-17 16:12:18 +0000

    UI revisited for question.

--- onyx-modules/quartz/quartz-core/src/main/java/org/obiba/onyx/quartz/core/wicket/layout/impl/SingleChoiceQuestionPanel.java
+++ onyx-modules/quartz/quartz-core/src/main/java/org/obiba/onyx/quartz/core/wicket/layout/impl/SingleChoiceQuestionPanel.java
@@ -18,19 +18,34 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.obiba.onyx.quartz.core.domain.answer.CategoryAnswer;
 import org.obiba.onyx.quartz.core.engine.questionnaire.question.Question;
 import org.obiba.onyx.quartz.core.engine.questionnaire.question.QuestionCategory;
+import org.obiba.onyx.quartz.core.service.ActiveQuestionnaireAdministrationService;
 import org.obiba.onyx.quartz.core.wicket.layout.QuestionPanel;
 import org.obiba.onyx.quartz.core.wicket.model.QuestionnaireStringResourceModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SingleChoiceQuestionPanel extends QuestionPanel {
 
   private static final long serialVersionUID = 2951128797454847260L;
 
+  private static final Logger log = LoggerFactory.getLogger(SingleChoiceQuestionPanel.class);
+
+  @SpringBean
+  private ActiveQuestionnaireAdministrationService activeQuestionnaireAdministrationService;
+
   @SuppressWarnings("serial")
   public SingleChoiceQuestionPanel(String id, Question question) {
     super(id, question);
 
+    if(question.getNumber() != null) {
+      add(new Label("number", question.getNumber()));
+    } else {
+      add(new Label("number"));
+    }
     add(new Label("label", new QuestionnaireStringResourceModel(question, "label", null)));
     add(new Label("instructions", new QuestionnaireStringResourceModel(question, "instructions", null)));
     add(new Label("caption", new QuestionnaireStringResourceModel(question, "caption", null)));
@@ -41,7 +56,7 @@ public class SingleChoiceQuestionPanel extends QuestionPanel {
 
       @Override
       protected void populateItem(ListItem item) {
-        QuestionCategory questionCategory = (QuestionCategory) item.getModelObject();
+        final QuestionCategory questionCategory = (QuestionCategory) item.getModelObject();
         Radio radio = new Radio("radio", item.getModel());
         radio.setLabel(new QuestionnaireStringResourceModel(questionCategory, "label", null));
 
@@ -64,16 +79,23 @@ public class SingleChoiceQuestionPanel extends QuestionPanel {
 
           @Override
           protected void onEvent(AjaxRequestTarget target) {
+            log.info("onchange.{}.{}", questionCategory.getQuestion().getName(), questionCategory.getCategory().getName());
             if(openField != null) {
               openField.setFieldEnabled(!openField.isFieldEnabled());
               target.addComponent(openField);
             }
+            // exclusive choice, only one answer per question
+            activeQuestionnaireAdministrationService.deleteAnswers(questionCategory.getQuestion());
+            // TODO get the open answer
+            activeQuestionnaireAdministrationService.answer(questionCategory, null);
           }
 
         });
 
         if(questionCategory.isSelected()) {
           radioGroup.setModel(item.getModel());
+          CategoryAnswer categoryAnswer = activeQuestionnaireAdministrationService.findAnswer(questionCategory);
+          activeQuestionnaireAdministrationService.answer(questionCategory, categoryAnswer != null ? categoryAnswer.getData() : null);
         }
       }
 
