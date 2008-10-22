006349a1020ceddbe9d071ac0d407250dc1ad48d
Alice Carey <alice.carey@mail.mcgill.ca>
2008-10-22 13:28:57 +0000

    bug fix pour le test unit QuartzPanelTest.java

--- onyx-modules/quartz/quartz-core/src/test/java/org/obiba/onyx/quartz/core/wicket/QuartzPanelTest.java
+++ onyx-modules/quartz/quartz-core/src/test/java/org/obiba/onyx/quartz/core/wicket/QuartzPanelTest.java
@@ -115,12 +115,13 @@ public class QuartzPanelTest {
     expect(activeQuestionnaireAdministrationServiceMock.startPage()).andReturn(questionnaire.getPages().get(0));
 
     // calls for Panel creation on next link click
-    expect(activeQuestionnaireAdministrationServiceMock.getQuestionnaire()).andReturn(questionnaire).times(23);
-    expect((questionnaireBundleManagerMock.getBundle("HealthQuestionnaire"))).andReturn(questionnaireBundleMock).times(18);
-    expect(activeQuestionnaireAdministrationServiceMock.getLanguage()).andReturn(Locale.FRENCH).times(18);
-    expect(questionnaireBundleMock.getMessageSource()).andReturn(messageSourceMock).times(17);
-    expect(questionnaireBundleMock.getPropertyKey((Questionnaire) EasyMock.anyObject(), (String) EasyMock.anyObject())).andReturn(new String()).times(17);
-    expect(messageSourceMock.getMessage("", null, Locale.FRENCH)).andReturn("").times(17);
+    expect(activeQuestionnaireAdministrationServiceMock.getQuestionnaire()).andReturn(questionnaire).anyTimes();
+    expect(questionnaireBundleMock.getQuestionnaire()).andReturn(questionnaire);
+    expect((questionnaireBundleManagerMock.getBundle("HealthQuestionnaire"))).andReturn(questionnaireBundleMock).anyTimes();
+    expect(activeQuestionnaireAdministrationServiceMock.getLanguage()).andReturn(Locale.FRENCH).anyTimes();
+    expect(questionnaireBundleMock.getMessageSource()).andReturn(messageSourceMock).anyTimes();
+    expect(questionnaireBundleMock.getPropertyKey((Questionnaire) EasyMock.anyObject(), (String) EasyMock.anyObject())).andReturn(new String()).anyTimes();
+    expect(messageSourceMock.getMessage("", null, Locale.FRENCH)).andReturn("").anyTimes();
 
     expect(activeQuestionnaireAdministrationServiceMock.findAnswer((QuestionCategory) EasyMock.anyObject())).andReturn(new CategoryAnswer()).times(3);
 
