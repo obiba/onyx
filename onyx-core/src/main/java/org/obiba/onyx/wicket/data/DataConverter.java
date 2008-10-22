721131dda484f8068a788a30b7615a95e090bed9
Yannick Marcon <yannick.marcon@obiba.org>
2008-10-22 18:42:45 +0000

    Case value is null or empty string, converted data is null (except for TEXT type).

--- onyx-core/src/main/java/org/obiba/onyx/wicket/data/DataConverter.java
+++ onyx-core/src/main/java/org/obiba/onyx/wicket/data/DataConverter.java
@@ -25,49 +25,52 @@ public class DataConverter implements IConverter {
   private static final long serialVersionUID = 3639500916194340039L;
 
   private static final Logger log = LoggerFactory.getLogger(DataConverter.class);
-  
+
   private DataType type;
 
   public DataConverter(DataType type) {
+    if(type == null) throw new IllegalArgumentException("DataType cannot be null.");
     this.type = type;
   }
 
   public Object convertToObject(String value, Locale locale) {
-    
+
     Data data = null;
-    
+
+    if(value == null || (!type.equals(DataType.TEXT) && value.length() == 0)) return null;
+
     try {
-    switch(type) {
-    case BOOLEAN:
-      data = new Data(type, Boolean.valueOf(value));
-      break;
+      switch(type) {
+      case BOOLEAN:
+        data = new Data(type, Boolean.valueOf(value));
+        break;
 
-    case DATE:
-      DateConverter dateConverter = new DateConverter();
-      data = new Data(type, (Serializable)dateConverter.convertToObject(value, locale));
-      break;
+      case DATE:
+        DateConverter dateConverter = new DateConverter();
+        data = new Data(type, (Serializable) dateConverter.convertToObject(value, locale));
+        break;
 
-    case DECIMAL:
-      data = new Data(type, Double.valueOf(value));
-      break;
+      case DECIMAL:
+        data = new Data(type, Double.valueOf(value));
+        break;
 
-    case INTEGER:
-      data = new Data(type, Long.valueOf(value));
-      break;
+      case INTEGER:
+        data = new Data(type, Long.valueOf(value));
+        break;
 
-    case TEXT:
-      data = new Data(type, value);
-      break;
+      case TEXT:
+        data = new Data(type, value);
+        break;
 
-    case DATA:
-      // TODO
-      break;
+      case DATA:
+        // TODO
+        break;
 
-    default:
-      break;
-    }
-    } catch (Exception ex) {
-      log.warn(ex.getMessage());
+      default:
+        break;
+      }
+    } catch(Exception ex) {
+      log.warn("{}: {}", ex.getClass().getSimpleName(), ex.getMessage());
       ConversionException cex = new ConversionException(ex.getMessage());
       throw cex;
     }
