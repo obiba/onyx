42ff7ae25854c812d6ce312fffd6748f97d689c3
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2008-11-05 20:03:09 +0000

    Added IBarcodePartParser javadoc comments. Changed return type of getPartTitle() to MessageSourceResolvable.

--- onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/domain/parser/IBarcodePartParser.java
+++ onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/domain/parser/IBarcodePartParser.java
@@ -15,11 +15,30 @@ import org.obiba.onyx.ruby.core.domain.BarcodePart;
 import org.springframework.context.MessageSourceResolvable;
 
 /**
- * Interface for barcode part parsers.
+ * Interface for <code>BarcodePart</code> parsers.
  */
 public interface IBarcodePartParser {
 
-  public String getPartTitle();
+  /**
+   * Returns the part's title, if it has one.
+   * 
+   * @return part title (<code>null</code> if none)
+   */
+  public MessageSourceResolvable getPartTitle();
 
-  public BarcodePart eatAndValidatePart(StringBuilder part, List<MessageSourceResolvable> errors);
+  /**
+   * Consumes the appropriate (parser-dependent) number of characters in the given barcode fragment, and creates a
+   * <code>BarcodePart</code> based on those characters.
+   * 
+   * The consumed characters are validated. Any errors found are added to the provided error list.
+   * 
+   * Note: Characters are consumed from left to right.
+   * 
+   * @param barcodeFragment a barcode fragment (for the first parser in a <code>BarcodeStructure</code> this will be a
+   * complete barcode; for subsequent parsers, this will be some fragment of the barcode -- i.e., whatever has not been
+   * consumed by earlier parsers)
+   * @param errors error list
+   * @return consumed barcode part (<code>null</code> in case of errors)
+   */
+  public BarcodePart eatAndValidatePart(StringBuilder barcodeFragment, List<MessageSourceResolvable> errors);
 }
