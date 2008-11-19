363b57b2bbcef1c2db37c8c60af6eed03c8d69f2
Philippe Laflamme <philippe.laflamme@obiba.org>
2008-11-19 18:51:17 +0000

    Added documentation to CustomFdfWriter class.

--- onyx-modules/marble/marble-core/src/main/java/com/lowagie/text/pdf/CustomFdfWriter.java
+++ onyx-modules/marble/marble-core/src/main/java/com/lowagie/text/pdf/CustomFdfWriter.java
@@ -19,9 +19,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * An extension of {@code FdfWriter} to support setting the {@code PdfName.A} entry in a field dictionary. The PDF
+ * standard supports {@code A} entries in the dictionary, but iText's FdfWriter does not support it.
+ * <p>
+ * This class offers an additional method {@link #setFieldAsAction(String, PdfAction)} that allows setting a field's
+ * {@code A} entry. The {@code PdfAction} can be constructed using one of the static method of the {@code PdfAction}
+ * class.
+ * 
  */
 public class CustomFdfWriter extends FdfWriter {
+
   private static final Logger log = LoggerFactory.getLogger(CustomFdfWriter.class);
 
   @Override
@@ -31,21 +38,21 @@ public class CustomFdfWriter extends FdfWriter {
   }
 
   public void setFieldAsAction(String field, PdfAction action) {
+    log.debug("Setting field {} as an A dictionary entry", field);
     setField(field, action);
   }
 
+  /**
+   * Overridden to handle the PdfAction type.
+   */
   static class MyWrt extends Wrt {
 
-    /**
-     * @param arg0
-     * @param arg1
-     * @throws IOException
-     */
-    MyWrt(OutputStream arg0, FdfWriter arg1) throws IOException {
-      super(arg0, arg1);
+    MyWrt(OutputStream os, FdfWriter fdf) throws IOException {
+      super(os, fdf);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     PdfArray calculate(HashMap map) throws IOException {
       PdfArray ar = new PdfArray();
       for(Iterator it = map.entrySet().iterator(); it.hasNext();) {
@@ -57,6 +64,7 @@ public class CustomFdfWriter extends FdfWriter {
         if(v instanceof HashMap) {
           dic.put(PdfName.KIDS, calculate((HashMap) v));
         } else if(v instanceof PdfAction) {
+          // Special case of the A entry.
           dic.put(PdfName.A, (PdfAction) v);
         } else {
           dic.put(PdfName.V, (PdfObject) v);
