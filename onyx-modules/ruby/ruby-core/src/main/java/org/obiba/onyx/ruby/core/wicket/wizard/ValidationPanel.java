56f62d36cea704a6f730d3907fd5ddc87c4be79b
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2008-11-20 15:21:47 +0000

    RUBY-7: Implemented validation panel (display of registered and expected tube counts).

--- onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/wicket/wizard/ValidationPanel.java
+++ onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/wicket/wizard/ValidationPanel.java
@@ -11,13 +11,22 @@ package org.obiba.onyx.ruby.core.wicket.wizard;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.util.value.ValueMap;
 import org.obiba.onyx.ruby.core.service.ActiveTubeRegistrationService;
+import org.obiba.onyx.wicket.model.SpringStringResourceModel;
 
 public class ValidationPanel extends Panel {
+  //
+  // Constants
+  //
+
+  private static final long serialVersionUID = 1L;
+
+  private static final String REGISTERED_TUBE_COUNT_RESOURCE_KEY = "Ruby.RegisteredTubeCount";
+
+  private static final String EXPECTED_TUBE_COUNT_RESOURCE_KEY = "Ruby.ExpectedTubeCount";
+
   //
   // Instance Variables
   //
@@ -32,7 +41,27 @@ public class ValidationPanel extends Panel {
   public ValidationPanel(String id) {
     super(id);
 
-    add(new Label("registeredTubeCount", new StringResourceModel("RegisteredTubeCount", this, new Model(new ValueMap("count=" + activeTubeRegistrationService.getRegisteredTubeCount())))));
-    add(new Label("expectedTubeCount", new StringResourceModel("ExpectedTubeCount", this, new Model(new ValueMap("count=" + activeTubeRegistrationService.getExpectedTubeCount())))));
+    add(new Label("registeredTubeCount", new PropertyModel(this, "registeredTubeCountLabelText")));
+    add(new Label("expectedTubeCount", new PropertyModel(this, "expectedTubeCountLabelText")));
+  }
+
+  //
+  // Methods
+  //
+
+  @SuppressWarnings("unused")
+  public String getRegisteredTubeCountLabelText() {
+    int registeredTubeCount = activeTubeRegistrationService.getRegisteredTubeCount();
+    SpringStringResourceModel model = new SpringStringResourceModel(REGISTERED_TUBE_COUNT_RESOURCE_KEY, new Object[] { registeredTubeCount }, REGISTERED_TUBE_COUNT_RESOURCE_KEY);
+
+    return model.getString();
+  }
+
+  @SuppressWarnings("unused")
+  public String getExpectedTubeCountLabelText() {
+    int expectedTubeCount = activeTubeRegistrationService.getExpectedTubeCount();
+    SpringStringResourceModel model = new SpringStringResourceModel(EXPECTED_TUBE_COUNT_RESOURCE_KEY, new Object[] { expectedTubeCount }, EXPECTED_TUBE_COUNT_RESOURCE_KEY);
+
+    return model.getString();
   }
 }
