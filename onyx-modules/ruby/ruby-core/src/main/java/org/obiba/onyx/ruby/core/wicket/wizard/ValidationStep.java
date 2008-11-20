3df2c322347951dda8d70d1253dc298394f12148
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2008-11-20 16:10:01 +0000

    RUBY-7: Added warning messages for too few / too many tubes registered.

--- onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/wicket/wizard/ValidationStep.java
+++ onyx-modules/ruby/ruby-core/src/main/java/org/obiba/onyx/ruby/core/wicket/wizard/ValidationStep.java
@@ -12,6 +12,9 @@ package org.obiba.onyx.ruby.core.wicket.wizard;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.obiba.onyx.ruby.core.service.ActiveTubeRegistrationService;
+import org.obiba.onyx.wicket.model.SpringStringResourceModel;
 import org.obiba.onyx.wicket.wizard.WizardForm;
 import org.obiba.onyx.wicket.wizard.WizardStepPanel;
 
@@ -21,6 +24,17 @@ public class ValidationStep extends WizardStepPanel {
   //
 
   private static final long serialVersionUID = 1L;
+
+  private static final String FEWER_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY = "Ruby.Warning.FewerTubesThanExpectedCollected";
+
+  private static final String MORE_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY = "Ruby.Warning.MoreTubesThanExpectedCollected";
+
+  //
+  // Instance Variables
+  //
+
+  @SpringBean
+  protected ActiveTubeRegistrationService activeTubeRegistrationService;
 
   //
   // Constructors
@@ -54,5 +68,23 @@ public class ValidationStep extends WizardStepPanel {
   @Override
   public void onStepOutPrevious(WizardForm form, AjaxRequestTarget target) {
     ((RubyWizardForm) form).setUpWizardFlow();
+  }
+
+  @Override
+  public void onStepInNext(WizardForm form, AjaxRequestTarget target) {
+    int registeredTubeCount = activeTubeRegistrationService.getRegisteredTubeCount();
+    int expectedTubeCount = activeTubeRegistrationService.getExpectedTubeCount();
+
+    String warningMessage = null;
+
+    if(registeredTubeCount < expectedTubeCount) {
+      warningMessage = new SpringStringResourceModel(FEWER_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY, new Object[] { registeredTubeCount, expectedTubeCount }, FEWER_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY).getString();
+    } else if(registeredTubeCount > expectedTubeCount) {
+      warningMessage = new SpringStringResourceModel(MORE_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY, new Object[] { registeredTubeCount, expectedTubeCount }, MORE_TUBES_THAN_EXPECTED_COLLECTED_RESOURCE_KEY).getString();
+    }
+
+    if(warningMessage != null) {
+      warn(warningMessage);
+    }
   }
 }
