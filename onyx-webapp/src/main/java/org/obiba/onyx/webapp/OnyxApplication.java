49368b4a16edf100d458991c4bff6d7e69eb242b
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-01-24 04:42:38 +0000

    Removed improper call to internal Wicket method.

--- onyx-webapp/src/main/java/org/obiba/onyx/webapp/OnyxApplication.java
+++ onyx-webapp/src/main/java/org/obiba/onyx/webapp/OnyxApplication.java
@@ -170,7 +170,6 @@ public class OnyxApplication extends WebApplication implements ISpringWebApplica
       throw new RuntimeException(e);
     }
 
-    super.internalInit();
   }
 
   protected void init() {
