25e87a7cab7ae58ce73d008cdb183decc9c0f29c
Yannick Marcon <yannick.marcon@obiba.org>
2009-01-26 21:23:41 +0000

    cartagene removed from package names of onyx-example.

--- onyx-example/src/main/java/org/obiba/onyx/example/questionnaire/util/CreateQuestionnaire.java
+++ onyx-example/src/main/java/org/obiba/onyx/example/questionnaire/util/CreateQuestionnaire.java
@@ -6,7 +6,7 @@
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  **********************************************************************************************************************/
-package org.obiba.onyx.cartagene.questionnaire.util;
+package org.obiba.onyx.example.questionnaire.util;
 
 import java.io.File;
 import java.io.IOException;
--- onyx-example/src/main/java/org/obiba/onyx/example/questionnaire/util/TestQuestionnaireContentBuilder.java
+++ onyx-example/src/main/java/org/obiba/onyx/example/questionnaire/util/TestQuestionnaireContentBuilder.java
@@ -7,7 +7,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package org.obiba.onyx.cartagene.questionnaire.util;
+package org.obiba.onyx.example.questionnaire.util;
 
 import java.util.List;
 
