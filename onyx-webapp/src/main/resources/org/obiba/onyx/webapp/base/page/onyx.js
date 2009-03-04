b745d44c1a587d3ea506482514ebe8745feed649
Yannick Marcon <yannick.marcon@obiba.org>
2009-03-04 13:35:17 +0000

    Base page styling.

--- onyx-webapp/src/main/resources/org/obiba/onyx/webapp/base/page/onyx.js
+++ onyx-webapp/src/main/resources/org/obiba/onyx/webapp/base/page/onyx.js
@@ -111,10 +111,7 @@ var outerLayout, innerLayout; // a var is required because this page utilizes: m
 //
 $(document).ready(function () {
 	outerLayout = $('body').layout( baseLayoutSettings );
-	//innerLayout = $('body > div.ui-layout-center > div#contentWrapper > wicket:child > wicket:extend > div.stage').layout( stageLayoutSettings );
 });
-
-
 
 //
 // Base Layout Settings
@@ -128,19 +125,5 @@ var baseLayoutSettings = {
 	north: {
 	    minSize: 0,
 		size: "auto"
-	},
-	center: {
-		onresize: "innerLayout.resizeAll"	// resize INNER LAYOUT when center pane resizes
-	}
-}
-
-//
-// Stage Layout Settings
-//
-var stageLayoutSettings = {
-	name: "stageLayout",
-	defaults: {
-		spacing_open: 0,
-		spacing_closed:0
 	}
 }
