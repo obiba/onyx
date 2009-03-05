497ed2bf83f9ee28577c3fb4cc55374bf3af07d8
Martin Boulanger <syrinx2004@gmail.com>
2009-03-05 19:23:07 +0000

    ONYX-311 : Merged ECG fix from 1.0.x

--- onyx-modules/jade/instrument-parent/interface-ecg-cardiosoft/src/main/java/org/obiba/onyx/jade/instrument/gehealthcare/BtrInputGenerator.java
+++ onyx-modules/jade/instrument-parent/interface-ecg-cardiosoft/src/main/java/org/obiba/onyx/jade/instrument/gehealthcare/BtrInputGenerator.java
@@ -80,7 +80,10 @@ public class BtrInputGenerator {
     putString(bb, inputData.get("INPUT_PARTICIPANT_LAST_NAME").getValueAsString()); // 0-30
     putString(bb, inputData.get("INPUT_PARTICIPANT_FIRST_NAME").getValueAsString()); // 31-61
 
-    bb.putShort(Short.valueOf(inputData.get("INPUT_PARTICIPANT_BIRTH_YEAR").getValueAsString())).putShort(Short.valueOf(inputData.get("INPUT_PARTICIPANT_BIRTH_MONTH").getValueAsString())).putShort(Short.valueOf(inputData.get("INPUT_PARTICIPANT_BIRTH_DAY").getValueAsString())); // 62-63,64-65,66-67
+    // We need to add one to the birthday month since the month of January is represented by "0" in
+    // java.util.Calendar (we need January to be "1" here).
+    short birthdayMonth = (short) (Short.parseShort(inputData.get("INPUT_PARTICIPANT_BIRTH_MONTH").getValueAsString()) + 1);
+    bb.putShort(Short.valueOf(inputData.get("INPUT_PARTICIPANT_BIRTH_YEAR").getValueAsString())).putShort(birthdayMonth).putShort(Short.valueOf(inputData.get("INPUT_PARTICIPANT_BIRTH_DAY").getValueAsString())); // 62-63,64-65,66-67
 
     putString(bb, inputData.get("INPUT_PARTICIPANT_BARCODE").getValueAsString()); // 68-98
     bb.putInt(Integer.valueOf(inputData.get("INPUT_PARTICIPANT_BARCODE").getValueAsString())); // 99-102
