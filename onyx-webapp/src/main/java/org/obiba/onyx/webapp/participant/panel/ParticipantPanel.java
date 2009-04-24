bd27946c2df8a97bbe71b34d0ae662b9a818512b
Alice Carey <alice.carey@mail.mcgill.ca>
2009-04-24 15:53:28 +0000

    ONYX-482
    Style modification for keyValueTable in modal window
    Implementation of the group attribute style in ParticipantPanel

--- onyx-webapp/src/main/java/org/obiba/onyx/webapp/participant/panel/ParticipantPanel.java
+++ onyx-webapp/src/main/java/org/obiba/onyx/webapp/participant/panel/ParticipantPanel.java
@@ -10,14 +10,22 @@
 package org.obiba.onyx.webapp.participant.panel;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.obiba.onyx.core.domain.participant.Gender;
+import org.obiba.onyx.core.domain.participant.Group;
 import org.obiba.onyx.core.domain.participant.Participant;
 import org.obiba.onyx.core.domain.participant.ParticipantAttribute;
 import org.obiba.onyx.core.domain.participant.ParticipantMetadata;
@@ -57,11 +65,9 @@ public class ParticipantPanel extends Panel {
     if(!shortList) {
       kvPanel.addRow(new StringResourceModel("Gender", this, null), new PropertyModel(this, "localizedGender"));
       kvPanel.addRow(new StringResourceModel("BirthDate", this, null), DateModelUtils.getDateModel(new PropertyModel(this, "dateFormat"), new PropertyModel(getModel(), "birthDate")));
-
-      for(ParticipantAttribute attribute : participantMetadata.getConfiguredAttributes()) {
-        String value = (participant.getConfiguredAttributeValue(attribute.getName()) != null) ? participant.getConfiguredAttributeValue(attribute.getName()).getValueAsString() : null;
-        kvPanel.addRow(new SpringStringResourceModel(new PropertyModel(attribute, "name")), new Model(value));
-      }
+      add(new AttributeGroupsFragment("configuredAttributeGroups", getModel()));
+    } else {
+      add(new EmptyPanel("configuredAttributeGroups"));
     }
 
     add(kvPanel);
@@ -74,5 +80,72 @@ public class ParticipantPanel extends Panel {
 
   public DateFormat getDateFormat() {
     return userSessionService.getDateFormat();
+  }
+
+  private class AttributeGroupsFragment extends Fragment {
+
+    private static final long serialVersionUID = 1L;
+
+    public AttributeGroupsFragment(String id, IModel participantModel) {
+      super(id, "attributeGroupsFragment", ParticipantPanel.this);
+
+      RepeatingView repeater = new RepeatingView("groupRepeater");
+      add(repeater);
+
+      List<Group> groups = getGroups();
+
+      for(int i = 0; i < groups.size(); i++) {
+        Group group = groups.get(i);
+
+        WebMarkupContainer item = new WebMarkupContainer(repeater.newChildId());
+        repeater.add(item);
+
+        String groupNameKey = group.getName();
+        String groupName = !group.isDefaultGroup() ? (new SpringStringResourceModel(groupNameKey, groupNameKey)).getString() : null;
+
+        item.add(new Label("groupName", groupName).setVisible(groupName != null));
+        item.add(new AttributeGroupFragment("group", participantModel, group));
+      }
+    }
+
+    private List<Group> getGroups() {
+      List<Group> groups = new ArrayList<Group>();
+
+      if(participantMetadata.getConfiguredAttributes().size() != 0) {
+        Group currentGroup = null;
+
+        for(ParticipantAttribute attribute : participantMetadata.getConfiguredAttributes()) {
+          Group group = attribute.getGroup();
+
+          if((currentGroup == null) || (group.getName() == null && currentGroup.getName() != null) || (group.getName() != null && currentGroup.getName() == null) || (group.getName() != null && currentGroup.getName() != null && !group.getName().equals(currentGroup.getName()))) {
+            groups.add(group);
+            currentGroup = group;
+          }
+        }
+      }
+
+      return groups;
+    }
+  }
+
+  private class AttributeGroupFragment extends Fragment {
+
+    private static final long serialVersionUID = 1L;
+
+    public AttributeGroupFragment(String id, IModel participantModel, Group group) {
+      super(id, "attributeGroupFragment", ParticipantPanel.this);
+
+      RepeatingView repeat = new RepeatingView("attributeRepeater");
+      add(repeat);
+
+      Participant participant = (Participant) participantModel.getObject();
+
+      for(final ParticipantAttribute attribute : group.getParticipantAttributes()) {
+        WebMarkupContainer item = new WebMarkupContainer(repeat.newChildId());
+        repeat.add(item);
+        item.add(new Label("label", new SpringStringResourceModel(new PropertyModel(attribute, "name"))));
+        item.add(new Label("field", new Model(participant.getConfiguredAttributeValue(attribute.getName()).getValueAsString())));
+      }
+    }
   }
 }
