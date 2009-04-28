8f7da55d90903627d68cf9cd9c609a334283922c
Tony DeBat <tony.debat@oicr.on.ca>
2009-04-28 15:00:44 +0000

    ONYX-538
    - Added FocusBehaviour to text field.

--- onyx-webapp/src/main/java/org/obiba/onyx/webapp/participant/panel/AddCommentPanel.java
+++ onyx-webapp/src/main/java/org/obiba/onyx/webapp/participant/panel/AddCommentPanel.java
@@ -9,6 +9,7 @@
  ******************************************************************************/
 package org.obiba.onyx.webapp.participant.panel;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -17,6 +18,7 @@ import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.validation.validator.StringValidator;
 import org.obiba.onyx.engine.Action;
+import org.obiba.onyx.wicket.behavior.FocusBehavior;
 import org.obiba.onyx.wicket.behavior.RequiredFormFieldBehavior;
 
 /**
@@ -50,6 +52,9 @@ public class AddCommentPanel extends Panel {
     add(new MultiLineLabel("instructions", new ResourceModel("AnonymousComments")));
 
     commentField = new TextArea("comment", new PropertyModel(getModelObject(), "comment"));
+    commentField.setOutputMarkupId(true);
+    commentField.add(new AttributeModifier("class", true, new Model("comment-text")));
+    commentField.add(new FocusBehavior());
     commentField.add(new RequiredFormFieldBehavior());
     commentField.add(new StringValidator.MaximumLengthValidator(2000));
 
