42a2af3103def0d3e8c53deae2f1e0dce2a76aa6
Yannick Marcon <yannick.marcon@obiba.org>
2009-06-11 23:09:56 +0000

    VariableHelper.addLocalizedAttributes added with MessageSourceResolvable.

--- onyx-core/src/main/java/org/obiba/onyx/engine/variable/VariableHelper.java
+++ onyx-core/src/main/java/org/obiba/onyx/engine/variable/VariableHelper.java
@@ -14,12 +14,21 @@ import java.util.Locale;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.NoSuchMessageException;
 
 /**
  * Helper class for provisioning a variable with localized attributes.
  */
 public class VariableHelper implements ApplicationContextAware {
+
+  public static final String VALIDATION = "validation";
+
+  public static final String CONDITION = "condition";
+
+  public static final String SOURCE = "source";
+
+  public static final String OCCURRENCE = "occurrence";
 
   private ApplicationContext applicationContext;
 
@@ -38,8 +47,7 @@ public class VariableHelper implements ApplicationContextAware {
    * @param property
    */
   public void addLocalizedAttributes(Variable variable, String property) {
-    // TODO get the list of available languages !
-    for(Locale locale : new Locale[] { Locale.ENGLISH, Locale.FRENCH }) {
+    for(Locale locale : getLocales()) {
       try {
         String message = applicationContext.getMessage(property, null, locale);
         if(message.trim().length() > 0) {
@@ -52,11 +60,56 @@ public class VariableHelper implements ApplicationContextAware {
   }
 
   /**
+   * Add "label" localized attributes for the given message source.
+   * @param variable
+   * @param resolvable
+   */
+  public void addLocalizedAttributes(Variable variable, MessageSourceResolvable resolvable) {
+    for(Locale locale : getLocales()) {
+      try {
+        String message = applicationContext.getMessage(resolvable, locale);
+        if(message.trim().length() > 0) {
+          variable.addAttributes(new Attribute("label", locale, message));
+        }
+      } catch(NoSuchMessageException ex) {
+        // ignore
+      }
+    }
+  }
+
+  /**
+   * Get the locales.
+   * @return
+   */
+  private Locale[] getLocales() {
+    // TODO get the list of available languages !
+    return new Locale[] { Locale.ENGLISH, Locale.FRENCH };
+  }
+
+  /**
    * Add "label" localized attributes from the variable name.
    * @param variable
    */
   public void addLocalizedAttributes(Variable variable) {
     addLocalizedAttributes(variable, variable.getName());
+  }
+
+  public void addConditionAttribute(Variable variable, Object source) {
+    addAttribute(variable, CONDITION, source);
+  }
+
+  public void addValidationAttribute(Variable variable, Object source) {
+    addAttribute(variable, VALIDATION, source);
+  }
+
+  public void addSourceAttribute(Variable variable, Object source) {
+    addAttribute(variable, SOURCE, source);
+  }
+
+  private void addAttribute(Variable variable, String key, Object source) {
+    if(source != null) {
+      variable.addAttributes(new Attribute(key, source.toString()));
+    }
   }
 
   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
