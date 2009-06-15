51379fbf67e3184797fe229d0fcd4c613e674161
Yannick Marcon <yannick.marcon@obiba.org>
2009-06-15 20:57:08 +0000

    Commented code uncommented.

--- onyx-core/src/main/java/org/obiba/onyx/engine/variable/export/OnyxDataExport.java
+++ onyx-core/src/main/java/org/obiba/onyx/engine/variable/export/OnyxDataExport.java
@@ -23,6 +23,7 @@ import org.hibernate.FlushMode;
 import org.hibernate.SessionFactory;
 import org.obiba.core.service.EntityQueryService;
 import org.obiba.onyx.core.domain.participant.Interview;
+import org.obiba.onyx.core.domain.participant.InterviewStatus;
 import org.obiba.onyx.core.domain.participant.Participant;
 import org.obiba.onyx.core.service.UserSessionService;
 import org.obiba.onyx.engine.variable.VariableDataSet;
@@ -105,7 +106,7 @@ public class OnyxDataExport {
       Participant participant = iterator.next();
       // Export completed interviews only
       Interview interview = participant.getInterview();
-      if(interview == null /* || interview.getStatus() != InterviewStatus.COMPLETED */) {
+      if(interview == null || interview.getStatus() != InterviewStatus.COMPLETED) {
         iterator.remove();
       }
     }
