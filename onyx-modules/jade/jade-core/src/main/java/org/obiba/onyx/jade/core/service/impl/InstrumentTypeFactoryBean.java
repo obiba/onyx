d49e5b2b49098214def495aa59583ca86bcafa4a
Martin Boulanger <syrinx2004@gmail.com>
2009-09-10 16:40:11 +0000

    Added ComparingDataSource to Jade config.

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/service/impl/InstrumentTypeFactoryBean.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/service/impl/InstrumentTypeFactoryBean.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
- * 
+ *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -27,6 +27,7 @@ import org.obiba.onyx.core.data.VariableDataSource;
 import org.obiba.onyx.core.domain.contraindication.Contraindication;
 import org.obiba.onyx.core.io.support.XStreamDataConverter;
 import org.obiba.onyx.jade.core.data.InstrumentParameterDataSource;
+import org.obiba.onyx.core.data.ComparingDataSource;
 import org.obiba.onyx.jade.core.domain.instrument.InstrumentInputParameter;
 import org.obiba.onyx.jade.core.domain.instrument.InstrumentOutputParameter;
 import org.obiba.onyx.jade.core.domain.instrument.InstrumentParameter;
@@ -152,7 +153,9 @@ public class InstrumentTypeFactoryBean implements FactoryBean, ApplicationContex
     xstream.alias("instrumentParameterDataSource", InstrumentParameterDataSource.class);
     xstream.alias("firstNotNullDataSource", FirstNotNullDataSource.class);
     xstream.alias("computingDataSource", ComputingDataSource.class);
-    xstream.alias("hashedDataSource", HashedDataSource.class);
+    xstream.alias("hashedDataSource", HashedDataSource.class);    ^
+    xstream.alias("comparingDataSource", ComparingDataSource.class);
+    xstream.useAttributeFor(ComparingDataSource.class, "comparisonOperator");
 
     xstream.alias("equalsValueCheck", EqualsValueCheck.class);
     xstream.alias("equalsParameterCheck", EqualsParameterCheck.class);
