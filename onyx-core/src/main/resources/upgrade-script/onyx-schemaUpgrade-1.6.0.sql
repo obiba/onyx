d2b90d7db6cd1a036fda4a01e6b995a0a7761991
Tony DeBat <tony.debat@oicr.on.ca>
2009-09-16 20:29:43 +0000

    - Fixed case. MySql is case sensitive.

--- onyx-core/src/main/resources/upgrade-script/onyx-schemaUpgrade-1.6.0.sql
+++ onyx-core/src/main/resources/upgrade-script/onyx-schemaUpgrade-1.6.0.sql
@@ -1,5 +1,5 @@
 -- Add a status column to Measure table (ONYX-701) 
-ALTER TABLE MEASURE ADD COLUMN STATUS VARCHAR(50);
+ALTER TABLE measure ADD COLUMN status VARCHAR(50);
 
 -- Add an export_date column to Participant table (ONYX-812)
-ALTER TABLE PARTICIPANT ADD COLUMN EXPORT_DATE DATETIME;
+ALTER TABLE participant ADD COLUMN export_date DATETIME;
