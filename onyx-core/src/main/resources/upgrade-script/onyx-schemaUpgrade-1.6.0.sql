25300cf6d0df77b25ba965cca7ced4d201e6acb6
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-09-18 14:16:06 +0000

    Replaced SQL-based schema upgrade step for 1.6.0 with one based on a reusable MethodInvokingUpgradeStep.

--- onyx-core/src/main/resources/upgrade-script/onyx-schemaUpgrade-1.6.0.sql
+++
@@ -1,5 +0,0 @@
--- Add a status column to Measure table (ONYX-701) 
-ALTER TABLE measure ADD COLUMN status VARCHAR(50);
-
--- Add an export_date column to Participant table (ONYX-812)
-ALTER TABLE participant ADD COLUMN export_date DATETIME;
