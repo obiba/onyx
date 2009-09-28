aa6a340a6060e6b97b89484b9e486cb4b59dfc99
Tony DeBat <tony.debat@oicr.on.ca>
2009-09-28 13:16:57 +0000

    - Removed commented code.

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/workstation/ActionsPanel.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/workstation/ActionsPanel.java
@@ -22,53 +22,35 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.value.ValueMap;
-import org.obiba.onyx.core.service.ActiveInterviewService;
 import org.obiba.onyx.jade.core.domain.instrument.Instrument;
 import org.obiba.onyx.jade.core.domain.instrument.InstrumentStatus;
-import org.obiba.onyx.jade.core.domain.workstation.InstrumentCalibration;
 import org.obiba.onyx.jade.core.service.ExperimentalConditionService;
 import org.obiba.onyx.wicket.reusable.Dialog.Status;
 import org.obiba.onyx.wicket.reusable.Dialog.WindowClosedCallback;
 import org.obiba.wicket.markup.html.border.SeparatorMarkupComponentBorder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ActionsPanel extends ExperimentalConditionDialog {
 
   private static final long serialVersionUID = 5855667390712874428L;
 
-  private static final Logger log = LoggerFactory.getLogger(ActionsPanel.class);
-
   @SpringBean
   private ExperimentalConditionService experimentalConditionService;
 
-  @SpringBean(name = "activeInterviewService")
-  private ActiveInterviewService activeInterviewService;
-
-  public ActionsPanel(String id, IModel stageModel) {
-    super(id, stageModel);
+  public ActionsPanel(String id, IModel<Instrument> model) {
+    super(id, model);
     setOutputMarkupId(true);
 
     RepeatingView repeating = new RepeatingView("link");
     add(repeating);
     SeparatorMarkupComponentBorder border = new SeparatorMarkupComponentBorder();
 
-    for(LinkInfo linkInfo : getListOfLinkInfo((Instrument) stageModel.getObject())) {
+    for(LinkInfo linkInfo : getListOfLinkInfo((Instrument) model.getObject())) {
       AjaxLink<LinkInfo> link = new AjaxLink<LinkInfo>(repeating.newChildId(), new Model<LinkInfo>(linkInfo)) {
         private static final long serialVersionUID = 1L;
 
         @Override
         public void onClick(AjaxRequestTarget target) {
-          // TODO Auto-generated method stub
-          System.out.println("***** clicke on calibrate " + getModelObject().name);
           getModelObject().onClick(target);
-          // if(actionDefinition != null) {
-          // modal.show(target, ActionsPanel.this.getDefaultModel(), actionDefinition);
-          // } else {
-          // log.warn("Concurrent interview administration. Session {} tried to execute ActionDefinition {} on stage {}, yet that ActionDefinition is not available for the current stage's state.",
-          // new Object[] { WebSession.get().getId(), getModelObject(), getStage() });
-          // setResponsePage(InterviewPage.class);
-          // }
         }
 
       };
@@ -78,41 +60,6 @@ public class ActionsPanel extends ExperimentalConditionDialog {
       repeating.add(link);
     }
 
-    // for(ActionDefinition actionDef : getStageExecution().getActionDefinitions()) {
-    //
-    // AjaxLink link = new AjaxLink(repeating.newChildId(), new Model(actionDef.getCode())) {
-    //
-    // private static final long serialVersionUID = 1L;
-    //
-    // @Override
-    // public void onClick(AjaxRequestTarget target) {
-    // // Before allowing the action, make sure our actionDefinition is sill available
-    // // in the IStageExecution. This protects concurrent interview administration.
-    // // See ONYX-154
-    // IStageExecution stageExec = getStageExecution();
-    // String code = (String) getModelObject();
-    // ActionDefinition actionDefinition = null;
-    // for(ActionDefinition definition : stageExec.getActionDefinitions()) {
-    // if(code.equals(definition.getCode())) {
-    // actionDefinition = definition;
-    // break;
-    // }
-    // }
-    // if(actionDefinition != null) {
-    // modal.show(target, ActionsPanel.this.getDefaultModel(), actionDefinition);
-    // } else {
-    // log.warn("Concurrent interview administration. Session {} tried to execute ActionDefinition {} on stage {}, yet that ActionDefinition is not available for the current stage's state.",
-    // new Object[] { WebSession.get().getId(), getModelObject(), getStage() });
-    // setResponsePage(InterviewPage.class);
-    // }
-    // }
-    //
-    // };
-    // link.add(new Label("action", new
-    // MessageSourceResolvableStringModel(actionDef.getLabel())).setRenderBodyOnly(true));
-    // link.setComponentBorder(border);
-    // repeating.add(link);
-    // }
   }
 
   private List<LinkInfo> getListOfLinkInfo(Instrument instrument) {
@@ -163,17 +110,6 @@ public class ActionsPanel extends ExperimentalConditionDialog {
       return instrument.getStatus().equals(InstrumentStatus.ACTIVE) && experimentalConditionService.instrumentCalibrationExists(instrument.getType());
     }
 
-    private InstrumentCalibration getInstrumentCalibration() {
-      InstrumentCalibration instrumentCalibration = null;
-      System.out.println("*** Instrument type is " + instrument.getType());
-      System.out.println("*** Instrument name is " + instrument.getName());
-      if(experimentalConditionService.instrumentCalibrationExists(instrument.getType())) {
-        System.out.println("**True!!!!!! ");
-        instrumentCalibration = experimentalConditionService.getInstrumentCalibrationByType(instrument.getType());
-      }
-      return instrumentCalibration;
-    }
-
     @Override
     public void onClick(AjaxRequestTarget target) {
       setExperimentalConditionLog(experimentalConditionService.getInstrumentCalibrationByType(instrument.getType()));
@@ -182,14 +118,7 @@ public class ActionsPanel extends ExperimentalConditionDialog {
         private static final long serialVersionUID = 1L;
 
         public void onClose(AjaxRequestTarget target, Status status) {
-          // TODO Refresh the instrument table.
-          System.out.println("** Refresh the instrument table.");
-          // ExperimentalConditionHistoryPanel newExperimentalConditionHistoryPanel =
-          // getExperimentalConditionHistoryPanel();
-          // experimentalConditionHistoryPanel.replaceWith(newExperimentalConditionHistoryPanel);
-          // experimentalConditionHistoryPanel = newExperimentalConditionHistoryPanel;
-          // if(experimentalConditionLogs.size() == 0) experimentalConditionHistoryPanel.setVisible(false);
-          // target.addComponent(experimentalConditionHistoryPanel);
+          // Refresh the instrument table.
         }
 
       });
@@ -256,5 +185,4 @@ public class ActionsPanel extends ExperimentalConditionDialog {
 
     }
   }
-
 }
