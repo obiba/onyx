2b2fc7af131e2adcd38f86c1077f98b03594e444
Tony DeBat <tony.debat@oicr.on.ca>
2009-10-13 19:19:29 +0000

    ONYX-635 -Added comments.

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/service/ExperimentalConditionService.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/service/ExperimentalConditionService.java
@@ -19,16 +19,32 @@ import org.obiba.onyx.jade.core.domain.workstation.ExperimentalConditionLog;
 import org.obiba.onyx.jade.core.domain.workstation.ExperimentalConditionValue;
 import org.obiba.onyx.jade.core.domain.workstation.InstrumentCalibration;
 
+/**
+ * This service provides methods to register and retrieve {@link ExperimentalConditionLog}s and
+ * {@link InstrumentCalibration}s as well as access the recorded calibration values themselves.
+ */
 public interface ExperimentalConditionService {
 
+  /**
+   * Persists an {@link ExperimentalCondition} and all its associated {@link ExperimentalConditionValues}.
+   * @param experimentalCondition ExperimentalCondition to persist.
+   */
   public void save(ExperimentalCondition experimentalCondition);
 
+  /**
+   * Returns a list all all the available {@link ExperimentalConditionLog}s.
+   * @return List of all ExperimentalConditionLogs.
+   */
   public List<ExperimentalConditionLog> getExperimentalConditionLog();
 
+  /**
+   * Register an {@link ExperimentalConditionLog} in order to make it available via this service.
+   * @param log The ExperimentalConditionLog to be registered.
+   */
   public void register(ExperimentalConditionLog log);
 
   /**
-   * Returns a list {@link ExperimentalConditions}. If all parameters are null, then all the ExperimentalConditions will
+   * Returns a list of {@link ExperimentalCondition}s. If all parameters are null, then all ExperimentalConditions will
    * be returned.
    * @param template Supply a template with a "name" and "workstation" to match on those values.
    * @param paging
@@ -37,14 +53,41 @@ public interface ExperimentalConditionService {
    */
   public List<ExperimentalCondition> getExperimentalConditions(ExperimentalCondition template, PagingClause paging, SortingClause... clauses);
 
+  /**
+   * Returns the {@link ExperimentalConditionLog} with the provided name.
+   * @param name The name of the ExperimentalConditionLog.
+   * @return
+   * @throws IllegalStateException If no ExperimentalLogCondition exists with the provided name.
+   */
   public ExperimentalConditionLog getExperimentalConditionLogByName(String name);
 
+  /**
+   * Returns true in an {@link InstrumentCalibration} exists for the given instrumentType.
+   * @param instrumentType
+   * @return True if an InstrumentCalibration exists for the instrumentType.
+   */
   public boolean instrumentCalibrationExists(String instrumentType);
 
+  /**
+   * Returns the {@link InstrumentCalibration} for the provided instrumentType.
+   * @param instrumentType
+   * @return
+   */
   public InstrumentCalibration getInstrumentCalibrationByType(String instrumentType);
 
+  /**
+   * Returns a list of {@link InstrumentCalibration}s for the given instrumentType.
+   * @param instrumentType
+   * @return A list of InstrumentCalibrations.
+   */
   public List<InstrumentCalibration> getInstrumentCalibrationsByType(String instrumentType);
 
+  /**
+   * Returns an {@link Attribute} for the given experimentalConditionValue.
+   * @param experimentalConditionValue
+   * @return
+   * @throws IllegalStateException If the attribute does not exist.
+   */
   public Attribute getAttribute(ExperimentalConditionValue experimentalConditionValue);
 
 }
