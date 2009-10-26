6206635ed9b32da5f11b029697ee2699bcd6c9dd
Martin Boulanger <syrinx2004@gmail.com>
2009-10-26 16:36:22 +0000

    ONYX-975 : Fixed InstrumentSelectionStep implementation.  The instrument barcode field was displaying the previously selected instrument.  This was already fixed in 1.6, but hasn't been retrofited in 1.5 yet.

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/wizard/InstrumentSelectionStep.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/core/wicket/wizard/InstrumentSelectionStep.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
- * 
+ *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -15,30 +15,38 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.obiba.onyx.core.service.ActiveInterviewService;
+import org.obiba.onyx.jade.core.domain.instrument.Instrument;
 import org.obiba.onyx.jade.core.service.ActiveInstrumentRunService;
 import org.obiba.onyx.jade.core.wicket.instrument.InstrumentSelector;
 import org.obiba.onyx.wicket.wizard.WizardForm;
 import org.obiba.onyx.wicket.wizard.WizardStepPanel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Instrument selection Step.
- * 
+ *
  */
 public class InstrumentSelectionStep extends WizardStepPanel {
 
   private static final long serialVersionUID = 4489598868219932761L;
 
+  private static final Logger log = LoggerFactory.getLogger(InstrumentSelectionStep.class);
+
   @SpringBean
   private ActiveInstrumentRunService activeInstrumentRunService;
 
-  @SuppressWarnings("serial")
+  private Instrument instrument;
+
   public InstrumentSelectionStep(String id, IModel instrumentTypeModel) {
     super(id);
     setOutputMarkupId(true);
 
     add(new Label(getTitleId(), new StringResourceModel("InstrumentSelection", this, null)));
 
-    add(new InstrumentSelector(getContentId(), instrumentTypeModel, new PropertyModel(activeInstrumentRunService, "instrumentRun.instrument")));
+    add(new InstrumentSelector(getContentId(), instrumentTypeModel, new PropertyModel(this, "instrument")));
+
   }
 
   @Override
@@ -51,6 +59,12 @@ public class InstrumentSelectionStep extends WizardStepPanel {
       target.addComponent(form.getPreviousLink());
       target.addComponent(form.getNextLink());
     }
+  }
+
+  @Override
+  public void onStepOutNext(WizardForm form, AjaxRequestTarget target) {
+    super.onStepOutNext(form, target);
+    activeInstrumentRunService.setInstrument(instrument);
   }
 
 }
