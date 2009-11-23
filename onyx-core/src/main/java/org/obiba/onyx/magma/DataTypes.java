935d1b488ca22f6bb7c78110b6977373b2519315
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-11-23 21:09:06 +0000

    Added DataType to ValueType converter

---
+++ onyx-core/src/main/java/org/obiba/onyx/magma/DataTypes.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.onyx.magma;
+
+import org.obiba.magma.ValueType;
+import org.obiba.magma.type.BinaryType;
+import org.obiba.magma.type.BooleanType;
+import org.obiba.magma.type.DateType;
+import org.obiba.magma.type.DecimalType;
+import org.obiba.magma.type.IntegerType;
+import org.obiba.magma.type.TextType;
+import org.obiba.onyx.util.data.DataType;
+
+public class DataTypes {
+
+  public static ValueType valueTypeFor(DataType dataType) {
+    if(dataType == null) throw new NullPointerException("dataType cannot be null.");
+    switch(dataType) {
+    case BOOLEAN:
+      return BooleanType.get();
+    case DATE:
+      return DateType.get();
+    case DATA:
+      return BinaryType.get();
+    case DECIMAL:
+      return DecimalType.get();
+    case INTEGER:
+      return IntegerType.get();
+    case TEXT:
+      return TextType.get();
+    }
+    throw new IllegalArgumentException("No ValueType for DataType '" + dataType + "'");
+  }
+
+}
