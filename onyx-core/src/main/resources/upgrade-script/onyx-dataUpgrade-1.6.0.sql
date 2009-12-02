31b72850f03feb82b24b31e3984307bffa21ec13
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-12-02 20:02:34 +0000

    ONYX-1004: Updated 1.6.0 upgrade procedure to modify existing measure rows as follows:   - set status to VALID   - set workstation to Unknown

--- onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
+++ onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
@@ -4,5 +4,8 @@ set export_date = sysdate()
 where exported = true
 and export_date is null;
 
+-- Set the status to VALID and workstation to Unknown for all existing measure rows (ONYX-1004)
+update measure set status = 'VALID', workstation = 'Unknown';
+
 -- For all consent rows, update the time_start column to the parent interview's start_date (ONYX-1003)
-UPDATE consent c, interview i SET c.time_start = i.start_date WHERE c.interview_id = i.id;
+update consent c, interview i SET c.time_start = i.start_date where c.interview_id = i.id;
