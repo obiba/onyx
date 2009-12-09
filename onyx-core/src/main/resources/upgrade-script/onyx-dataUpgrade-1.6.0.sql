f5ed69f18ce6f7e37b31c86b22e5ad56975c3d59
Alice Carey <alice.carey@mail.mcgill.ca>
2009-12-09 15:54:44 +0000

    ONYX-1049

--- onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
+++ onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
@@ -9,3 +9,6 @@ update measure set status = 'VALID', workstation = 'Unknown';
 
 -- For all consent rows, update the time_start column to the parent interview's start_date (ONYX-1003)
 update consent c, interview i SET c.time_start = i.start_date where c.interview_id = i.id;
+
+-- ONYX-1049 In the improbable case that an instrument contains an empty string as barcode, which should not be possible.
+update instrument set barcode = CONCAT(type, '_barcode') where barcode = "";
