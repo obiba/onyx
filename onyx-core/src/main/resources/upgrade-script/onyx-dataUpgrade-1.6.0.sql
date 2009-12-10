f2d50a5ee009d0be1941a7f290b5758c8d32af03
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-12-10 18:07:46 +0000

    ONYX-1053: Removed single quote for comment in SQL upgrade script.

--- onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
+++ onyx-core/src/main/resources/upgrade-script/onyx-dataUpgrade-1.6.0.sql
@@ -1,8 +1,8 @@
 -- Set the status to VALID and workstation to Unknown for all existing measure rows (ONYX-1004)
 update measure set status = 'VALID', workstation = 'Unknown';
 
--- For all consent rows, update the time_start column to the parent interview's start_date (ONYX-1003)
+-- For all consent rows, update the time_start column to the parent interview start_date (ONYX-1003)
 update consent c, interview i SET c.time_start = i.start_date where c.interview_id = i.id;
 
--- ONYX-1049 In the improbable case that an instrument contains an empty string as barcode, which should not be possible.
+-- In the improbable case that an instrument contains an empty string as barcode, which should not be possible (ONYX-1049)
 update instrument set barcode = CONCAT(type, '_barcode') where barcode = '';
