f2d4b1abb5c43cad7a893610f73ba1f3d2e96369
Yannick Marcon <yannick.marcon@obiba.org>
2010-01-21 15:32:49 +0000

    ONYX-1148 Check of number of selected reports before displaying info message.

--- onyx-core/src/main/java/org/obiba/onyx/wicket/reusable/PrintableReportPanel.java
+++ onyx-core/src/main/java/org/obiba/onyx/wicket/reusable/PrintableReportPanel.java
@@ -279,8 +279,11 @@ public class PrintableReportPanel extends Panel {
       log.error("Unable to print the report [" + store.reportName + "]. ", e);
       return;
     }
-    StringResourceModel successMessageModel = new StringResourceModel("printSuccessMessage", this, new Model(""));
-    info(successMessageModel.getString());
+    log.info("checkedReports.size()={}", checkedReports.size());
+    if(checkedReports.size() > 0) {
+      StringResourceModel successMessageModel = new StringResourceModel("printSuccessMessage", this, new Model(""));
+      info(successMessageModel.getString());
+    }
   }
 
   public IFormValidator getFormValidator() {
