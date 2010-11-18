3ef774ef84b1e3f13e6895c8c0ea4baef7278bc0
Clément Tamisier <ctamisier@p3g.org>
2010-11-18 20:16:08 +0000

    fix ONYX-1356

--- onyx-modules/quartz/quartz-core/src/main/resources/org/obiba/onyx/quartz/editor/questionnaire/tree/QuestionnaireTreePanel.js
+++ onyx-modules/quartz/quartz-core/src/main/resources/org/obiba/onyx/quartz/editor/questionnaire/tree/QuestionnaireTreePanel.js
@@ -48,57 +48,67 @@ Wicket.QTree.buildTree = function(treeId, jsonUrl) {
 				}				
 			}
 		},
-		"contextmenu" : {
-			"select_node": true,
-			"items": 	
-				function (node) { 
-					var elementId = node.attr("id");
-					var elementType = node.attr("rel");
-					var obj = { 
-						"create_section": (elementType != "Questionnaire" && elementType != "Section") ? false : { 
-							label: "Add new Section", 
-							action: function (obj) { Wicket.QTree.addChild(elementId, 'section'); }, 
-							seperator_after : false, 
-							seperator_before : false 
-						}, 
-						"create_page": (elementType != "Section") ? false : { 
-							label: "Add new Page", 
-							action: function (obj) { Wicket.QTree.addChild(elementId, 'page'); },
-							seperator_after : false, 
-							seperator_before : false 
-						}, 
-						"create_question": (elementType != "Page") ? false :{ 
-							label: "Add new Question", 
-							action: function (obj) { Wicket.QTree.addChild(elementId, 'question'); },
-							seperator_after : false, 
-							seperator_before : false 
-						},
-						"create_variable": (elementType != "Variables") ? false :{ 
-							label: "Add new Variable", 
-							action: function (obj) { Wicket.QTree.addChild(elementId, 'variable'); },
-							seperator_after : false, 
-							seperator_before : false 
-						},
-						"edit": elementType == "Variables" ? false : { 
-							label: "Edit", 
-							action: function (obj) { Wicket.QTree.editElement(elementId); },
-							seperator_after : false, 
-							seperator_before : true 
-						},
-						"delete": (elementType == "Variables" || elementType == "Questionnaire") ? false : { 
-							label: "Delete", 
-							action: function (obj) { Wicket.QTree.deleteElement(elementId); },
-							seperator_after : false, 
-							seperator_before : true 
-						}
-					}
-					return obj; 
-				}
-		 }, 
+//		"contextmenu" : {
+//			"select_node": true,
+//			"items": 	
+//				function (node) { 
+//					var elementId = node.attr("id");
+//					var elementType = node.attr("rel");
+//					var obj = { 
+//						"create_section": (elementType != "Questionnaire" && elementType != "Section") ? false : { 
+//							label: "Add new Section", 
+//							action: function (obj) { Wicket.QTree.addChild(elementId, 'section'); }, 
+//							seperator_after : false, 
+//							seperator_before : false 
+//						}, 
+//						"create_page": (elementType != "Section") ? false : { 
+//							label: "Add new Page", 
+//							action: function (obj) { Wicket.QTree.addChild(elementId, 'page'); },
+//							seperator_after : false, 
+//							seperator_before : false 
+//						}, 
+//						"create_question": (elementType != "Page") ? false :{ 
+//							label: "Add new Question", 
+//							action: function (obj) { Wicket.QTree.addChild(elementId, 'question'); },
+//							seperator_after : false, 
+//							seperator_before : false 
+//						},
+//						"create_variable": (elementType != "Variables") ? false :{ 
+//							label: "Add new Variable", 
+//							action: function (obj) { Wicket.QTree.addChild(elementId, 'variable'); },
+//							seperator_after : false, 
+//							seperator_before : false 
+//						},
+//						"edit": elementType == "Variables" ? false : { 
+//							label: "Edit", 
+//							action: function (obj) { Wicket.QTree.editElement(elementId); },
+//							seperator_after : false, 
+//							seperator_before : true 
+//						},
+//						"delete": (elementType == "Variables" || elementType == "Questionnaire") ? false : { 
+//							label: "Delete", 
+//							action: function (obj) { Wicket.QTree.deleteElement(elementId); },
+//							seperator_after : false, 
+//							seperator_before : true 
+//						}
+//					}
+//					return obj; 
+//				}
+//		 }, 
 		"ui" : {
 			"select_limit" : 1
 		},
-		"plugins" : [ "themes", "json_data", "types", "ui", "dnd" /*, "contextmenu" */] // don't use contextmenu because menu does not hide on mout out
+		"crrm" : {
+			"move" : {
+				"check_move" : function (m) {
+					if (m.o[0].getAttribute("rel") == "Variable") {
+						return false;
+					}
+					return true;
+				}
+			}
+		},		
+		"plugins" : [ "themes", "json_data", "types", "ui", "dnd","crrm" /*, "contextmenu" */] // don't use contextmenu because menu does not hide on mouse out
 	})
 	.bind("move_node.jstree", function (e, data) {
 		data.rslt.o.each(function (i) {
