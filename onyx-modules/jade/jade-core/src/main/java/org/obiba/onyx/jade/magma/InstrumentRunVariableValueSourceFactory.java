ccb8865b2691cc7da8112ebf66030cc763ac65b3
Philippe Laflamme <philippe.laflamme@obiba.org>
2011-12-14 17:52:55 +0000

    Fixed variable ordering issue

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/magma/InstrumentRunVariableValueSourceFactory.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/magma/InstrumentRunVariableValueSourceFactory.java
@@ -9,7 +9,6 @@
  ******************************************************************************/
 package org.obiba.onyx.jade.magma;
 
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -152,7 +151,7 @@ public class InstrumentRunVariableValueSourceFactory implements VariableValueSou
   }
 
   private Set<VariableValueSource> createInstrumentParameterSources(InstrumentType instrumentType) {
-    Set<VariableValueSource> sources = new HashSet<VariableValueSource>();
+    Set<VariableValueSource> sources = new LinkedHashSet<VariableValueSource>();
 
     List<InstrumentParameter> instrumentParameters = instrumentType.getInstrumentParameters();
     if(instrumentParameters.size() > 0) {
