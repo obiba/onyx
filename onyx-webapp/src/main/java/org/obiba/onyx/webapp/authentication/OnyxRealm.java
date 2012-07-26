27b792c5af5e4dc982dc8550eaa415b6fe3b803e
Yannick Marcon <yannick.marcon@obiba.org>
2012-07-26 13:58:56 +0000

    ONYX-301 Onyx realm does not depend on persistence manager anymore.

--- onyx-webapp/src/main/java/org/obiba/onyx/webapp/authentication/OnyxRealm.java
+++ onyx-webapp/src/main/java/org/obiba/onyx/webapp/authentication/OnyxRealm.java
@@ -9,6 +9,12 @@
  ******************************************************************************/
 package org.obiba.onyx.webapp.authentication;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Locale;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -16,13 +22,16 @@ import org.apache.shiro.authc.DisabledAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
-import org.obiba.core.service.EntityQueryService;
-import org.obiba.onyx.core.domain.user.User;
+import org.apache.shiro.util.JdbcUtils;
+import org.obiba.onyx.core.domain.user.Status;
 import org.obiba.onyx.webapp.OnyxAuthenticatedSession;
 
 public class OnyxRealm extends JdbcRealm {
 
-  private EntityQueryService queryService;
+  /**
+   * The default query used to retrieve account status for the user.
+   */
+  protected static final String USER_PROPERTIES_QUERY = "select status,language from user where login = ? and deleted = 0";
 
   public OnyxRealm() {
     super();
@@ -35,21 +44,66 @@ public class OnyxRealm extends JdbcRealm {
   protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
     AuthenticationInfo info = super.doGetAuthenticationInfo(token);
 
-    User template = new User();
-    template.setLogin(((UsernamePasswordToken) token).getUsername());
-    template.setDeleted(false);
-    User fetchedUser = queryService.matchOne(template);
-    if(fetchedUser.isActive() == false) {
-      throw new DisabledAccountException();
-    }
-    if(fetchedUser.getLanguage() != null) {
-      OnyxAuthenticatedSession.get().setLocale(fetchedUser.getLanguage());
+    Connection conn = null;
+    try {
+      conn = dataSource.getConnection();
+      UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+      String username = upToken.getUsername();
+      String[] result = getPropertiesForUser(conn, username);
+      if(Status.INACTIVE.toString().equals(result[0])) {
+        throw new DisabledAccountException();
+      }
+      if(result[1] != null) {
+        OnyxAuthenticatedSession.get().setLocale(new Locale(result[1]));
+      }
+    } catch(SQLException e) {
+      throw new AuthenticationException(e);
+    } finally {
+      JdbcUtils.closeConnection(conn);
     }
 
     return info;
   }
 
-  public void setQueryService(EntityQueryService queryService) {
-    this.queryService = queryService;
+  /**
+   * 
+   * @param conn
+   * @param username
+   * @return status,language
+   * @throws SQLException
+   */
+  private String[] getPropertiesForUser(Connection conn, String username) throws SQLException {
+
+    String[] result = new String[] { Status.INACTIVE.toString(), null };
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    try {
+      ps = conn.prepareStatement(USER_PROPERTIES_QUERY);
+      ps.setString(1, username);
+
+      // Execute query
+      rs = ps.executeQuery();
+
+      // Loop over results - although we are only expecting one result, since usernames should be unique
+      boolean foundResult = false;
+      while(rs.next()) {
+
+        // Check to ensure only one row is processed
+        if(foundResult) {
+          throw new AuthenticationException("More than one user row found for user [" + username + "]. Usernames must be unique.");
+        }
+
+        result[0] = rs.getString(1);
+        result[1] = rs.getString(2);
+
+        foundResult = true;
+      }
+    } finally {
+      JdbcUtils.closeResultSet(rs);
+      JdbcUtils.closeStatement(ps);
+    }
+
+    return result;
   }
+
 }
