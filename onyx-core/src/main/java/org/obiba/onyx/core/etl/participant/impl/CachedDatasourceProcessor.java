9932ffe6262005cab7e0fde28a19e6fb2c496fd5
Yannick Marcon <yannick.marcon@obiba.org>
2015-06-10 10:07:45 -0400

    Allow specifying which tables are to be cached at appointment list update

--- onyx-core/src/main/java/org/obiba/onyx/core/etl/participant/impl/CachedDatasourceProcessor.java
+++ onyx-core/src/main/java/org/obiba/onyx/core/etl/participant/impl/CachedDatasourceProcessor.java
@@ -2,6 +2,9 @@ package org.obiba.onyx.core.etl.participant.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.obiba.magma.Datasource;
 import org.obiba.magma.MagmaCacheExtension;
@@ -13,6 +16,7 @@ import org.obiba.magma.VariableEntity;
 import org.obiba.magma.support.AbstractDatasourceWrapper;
 import org.obiba.magma.support.CachedDatasource;
 import org.obiba.magma.support.CachedValueTable;
+import org.obiba.magma.support.MagmaEngineTableResolver;
 import org.obiba.magma.support.VariableEntityBean;
 import org.obiba.magma.type.BinaryType;
 import org.obiba.onyx.core.domain.participant.Participant;
@@ -23,14 +27,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ExecutionContext;
 
+import com.google.common.collect.Maps;
+
 public class CachedDatasourceProcessor implements IParticipantPostProcessor {
 
   private static final Logger log = LoggerFactory.getLogger(CachedDatasourceProcessor.class);
 
   private MagmaEngine magmaEngine;
 
+  /**
+   * Restrict caching warm up to the specified tables (no restriction if nothing is specified).
+   */
+  private Map<String, Set<String>> datasourceTables = Maps.newHashMap();
+
   public void setMagmaEngine(MagmaEngine magmaEngine) {
     this.magmaEngine = magmaEngine;
+  }
+
+  public void setTableNames(String[] tableNames) {
+    if(tableNames == null || tableNames.length == 0) return;
+
+    for(String tableName : tableNames) {
+      MagmaEngineTableResolver resolver = MagmaEngineTableResolver.valueOf(tableName);
+      if(!datasourceTables.containsKey(resolver.getDatasourceName())) {
+        datasourceTables.put(resolver.getDatasourceName(), new TreeSet<String>());
+      }
+      datasourceTables.get(resolver.getDatasourceName()).add(resolver.getTableName());
+    }
   }
 
   @Override
@@ -46,7 +69,12 @@ public class CachedDatasourceProcessor implements IParticipantPostProcessor {
   }
 
   protected boolean isApplicable(Datasource datasource) {
-    return true;
+    return datasourceTables.isEmpty() || datasourceTables.containsKey(datasource.getName());
+  }
+
+  protected boolean isApplicable(Datasource datasource, ValueTable table) {
+    return datasourceTables.isEmpty() || datasourceTables.containsKey(datasource.getName()) &&
+        datasourceTables.get(datasource.getName()).contains(table.getName());
   }
 
   protected boolean isApplicable(ValueTable table, Variable variable) {
@@ -67,9 +95,9 @@ public class CachedDatasourceProcessor implements IParticipantPostProcessor {
   private void doCache(ExecutionContext context, CachedDatasource datasource, List<Participant> participants) {
     try {
       for(ValueTable table : datasource.getValueTables()) {
-        doCache(context, (CachedValueTable) table, participants);
+        if(isApplicable(datasource, table)) doCache(context, (CachedValueTable) table, participants);
       }
-    } catch (Exception e) {
+    } catch(Exception e) {
       String msg = "Unable to get tables of datasource: " + datasource.getName();
       log(context, AppointmentUpdateLog.Level.ERROR, e, msg);
     }
@@ -80,6 +108,8 @@ public class CachedDatasourceProcessor implements IParticipantPostProcessor {
 
     if(!table.isForEntityType(MagmaInstanceProvider.PARTICIPANT_ENTITY_TYPE)) return;
 
+    log(context, AppointmentUpdateLog.Level.INFO,
+        "Caching data from: " + table.getDatasource().getName() + "." + table.getName());
     Iterable<Variable> variables = table.getVariables();
 
     for(Participant participant : participants) {
@@ -89,7 +119,8 @@ public class CachedDatasourceProcessor implements IParticipantPostProcessor {
 
   private void doCache(ExecutionContext context, CachedValueTable table, Iterable<Variable> variables,
       Participant participant) {
-    VariableEntity entity = new VariableEntityBean(MagmaInstanceProvider.PARTICIPANT_ENTITY_TYPE, participant.getEnrollmentId());
+    VariableEntity entity = new VariableEntityBean(MagmaInstanceProvider.PARTICIPANT_ENTITY_TYPE,
+        participant.getEnrollmentId());
 
     table.evictValues(entity);
 
@@ -105,7 +136,8 @@ public class CachedDatasourceProcessor implements IParticipantPostProcessor {
           }
         }
       } catch(Exception e) {
-        String msg = "Unable to cache data of " + MagmaInstanceProvider.PARTICIPANT_ENTITY_TYPE + ": " + participant.getEnrollmentId();
+        String msg = "Unable to cache data of " + MagmaInstanceProvider.PARTICIPANT_ENTITY_TYPE + ": " +
+            participant.getEnrollmentId();
         log(context, AppointmentUpdateLog.Level.ERROR, e, msg);
       }
     }
