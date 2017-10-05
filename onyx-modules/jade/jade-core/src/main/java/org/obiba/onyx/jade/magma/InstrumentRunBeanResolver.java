d71d2101a16c14d0a90395c6e20881ce339d40e0
Yannick Marcon <yannick.marcon@obiba.org>
2017-10-05 10:08:54 +0200

    ONYX-1748 Fix NPE issue

--- onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/magma/InstrumentRunBeanResolver.java
+++ onyx-modules/jade/jade-core/src/main/java/org/obiba/onyx/jade/magma/InstrumentRunBeanResolver.java
@@ -1,17 +1,13 @@
 /*******************************************************************************
  * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
- * 
+ *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.obiba.onyx.jade.magma;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import org.obiba.magma.ValueSet;
 import org.obiba.magma.Variable;
@@ -30,6 +26,9 @@ import org.obiba.onyx.magma.StageAttributeVisitor;
 import org.obiba.onyx.util.StringUtil;
 import org.obiba.onyx.util.data.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ValueSetBeanResolver for InstrumentRun beans.
@@ -62,31 +61,31 @@ public class InstrumentRunBeanResolver extends AbstractOnyxBeanResolver {
   }
 
   public Object resolve(Class<?> type, ValueSet valueSet, Variable variable) throws NoSuchBeanException {
-    if(type.equals(InstrumentRun.class)) {
+    if (type.equals(InstrumentRun.class)) {
       return resolveInstrumentRun(valueSet, variable);
-    } else if(type.equals(InstrumentRunValue.class)) {
+    } else if (type.equals(InstrumentRunValue.class)) {
       String firstToken = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 0);
-      if(firstToken != null) {
-        if(firstToken.equals(InstrumentRunVariableValueSourceFactory.MEASURE)) {
+      if (firstToken != null) {
+        if (firstToken.equals(InstrumentRunVariableValueSourceFactory.MEASURE)) {
           return resolveInstrumentRunValues(valueSet, variable);
         } else {
           return resolveInstrumentRunValue(valueSet, variable);
         }
       }
-    } else if(type.equals(Measure.class)) {
+    } else if (type.equals(Measure.class)) {
       return resolveMeasure(valueSet, variable);
-    } else if(type.equals(Data.class)) {
+    } else if (type.equals(Data.class)) {
       String firstToken = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 0);
-      if(firstToken != null) {
-        if(firstToken.equals(InstrumentRunVariableValueSourceFactory.MEASURE)) {
+      if (firstToken != null) {
+        if (firstToken.equals(InstrumentRunVariableValueSourceFactory.MEASURE)) {
           return resolveDatas(valueSet, variable);
         } else {
           return resolveData(valueSet, variable);
         }
       }
-    } else if(type.equals(Contraindication.class)) {
+    } else if (type.equals(Contraindication.class)) {
       return resolveContraindication(valueSet, variable);
-    } else if(type.equals(Measure.class)) {
+    } else if (type.equals(Measure.class)) {
       return resolveMeasure(valueSet, variable);
     }
 
@@ -115,130 +114,110 @@ public class InstrumentRunBeanResolver extends AbstractOnyxBeanResolver {
 
   protected InstrumentRun resolveInstrumentRun(ValueSet valueSet, Variable variable) {
     String instrumentTypeName = variable.getAttributeStringValue(StageAttributeVisitor.STAGE_ATTRIBUTE);
-    if(instrumentTypeName != null) {
-      Participant participant = getParticipant(valueSet);
-      if(participant != null) {
-        return instrumentRunService.getInstrumentRun(participant, instrumentTypeName);
-      }
-    }
-    return null;
+    if (instrumentTypeName == null) return null;
+
+    Participant participant = getParticipant(valueSet);
+    return participant == null ? null : instrumentRunService.getInstrumentRun(participant, instrumentTypeName);
   }
 
   protected InstrumentRunValue resolveInstrumentRunValue(ValueSet valueSet, Variable variable) {
     InstrumentRun instrumentRun = resolveInstrumentRun(valueSet, variable);
-    if(instrumentRun != null) {
-      String parameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 0);
-      if(parameterCode != null) {
-        for(InstrumentRunValue runValue : instrumentRun.getInstrumentRunValues()) {
-          if(runValue.getInstrumentParameter().equals(parameterCode)) {
-            return runValue;
-          }
-        }
+    if (instrumentRun == null) return null;
+
+    String parameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 0);
+    if (parameterCode == null) return null;
+
+    for (InstrumentRunValue runValue : instrumentRun.getInstrumentRunValues()) {
+      if (runValue.getInstrumentParameter().equals(parameterCode)) {
+        return runValue;
       }
     }
+
     return null;
   }
 
   protected List<InstrumentRunValue> resolveInstrumentRunValues(ValueSet valueSet, Variable variable) {
     InstrumentRun instrumentRun = resolveInstrumentRun(valueSet, variable);
-    if(instrumentRun != null) {
-      String parameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 1);
-      if(parameterCode != null) {
-        List<InstrumentRunValue> values = new ArrayList<InstrumentRunValue>();
-        for(Measure measure : instrumentRun.getMeasures()) {
-          for(InstrumentRunValue runValue : measure.getInstrumentRunValues()) {
-            if(runValue.getInstrumentParameter().equals(parameterCode)) {
-              values.add(runValue);
-            }
-          }
+    // ONYX-1748
+    if (instrumentRun == null) return null;
+
+    String parameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 1);
+    if (parameterCode == null) return null;
+
+    List<InstrumentRunValue> values = new ArrayList<InstrumentRunValue>();
+    for (Measure measure : instrumentRun.getMeasures()) {
+      for (InstrumentRunValue runValue : measure.getInstrumentRunValues()) {
+        if (runValue.getInstrumentParameter().equals(parameterCode)) {
+          values.add(runValue);
         }
-        return values;
       }
     }
-    // ONYX-1748
-    return null;
+    return values;
   }
 
   protected List<Measure> resolveMeasure(ValueSet valueSet, Variable variable) {
-    InstrumentRun instrumentRun = (InstrumentRun) resolveInstrumentRun(valueSet, variable);
-    if(instrumentRun != null) {
-      return instrumentRun.getMeasures();
-    }
-    return null;
+    InstrumentRun instrumentRun = resolveInstrumentRun(valueSet, variable);
+    return instrumentRun == null ? null : instrumentRun.getMeasures();
   }
 
   protected InstrumentRunValue getInstrumentRunValue(ValueSet valueSet, InstrumentType instrumentType, InstrumentParameter instrumentParameter) {
     Participant participant = getParticipant(valueSet);
-    if(participant != null) {
-      return instrumentRunService.getInstrumentRunValue(participant, instrumentType.getName(), instrumentParameter.getCode(), null);
-    }
-    return null;
+    return participant == null ? null :
+        instrumentRunService.getInstrumentRunValue(participant, instrumentType.getName(), instrumentParameter.getCode(), null);
   }
 
   protected Data resolveData(ValueSet valueSet, Variable variable) {
     String instrumentTypeName = variable.getAttributeStringValue(StageAttributeVisitor.STAGE_ATTRIBUTE);
     String instrumentParameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 0);
 
-    if(instrumentTypeName != null && instrumentParameterCode != null) {
-      InstrumentRunValue instrumentRunValue = resolveInstrumentRunValue(valueSet, variable);
+    if (instrumentTypeName == null || instrumentParameterCode == null) return null;
+    InstrumentRunValue instrumentRunValue = resolveInstrumentRunValue(valueSet, variable);
+    if (instrumentRunValue == null) return null;
 
-      if(instrumentRunValue != null) {
-        InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
+    InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
+    if (instrumentType == null) return null;
 
-        if(instrumentType != null) {
-          InstrumentParameter instrumentParameter = instrumentType.getInstrumentParameter(instrumentParameterCode);
+    InstrumentParameter instrumentParameter = instrumentType.getInstrumentParameter(instrumentParameterCode);
+    if (instrumentParameter == null) return null;
 
-          if(instrumentParameter != null) {
-            return instrumentRunValue.getData(instrumentParameter.getDataType());
-          }
-        }
-      }
-    }
-
-    return null;
+    return instrumentRunValue.getData(instrumentParameter.getDataType());
   }
 
   protected List<Data> resolveDatas(ValueSet valueSet, Variable variable) {
     String instrumentTypeName = variable.getAttributeStringValue(StageAttributeVisitor.STAGE_ATTRIBUTE);
     String instrumentParameterCode = StringUtil.splitAndReturnTokenAt(variable.getName(), "\\.", 1);
 
-    if(instrumentTypeName != null && instrumentParameterCode != null) {
-      List<InstrumentRunValue> instrumentRunValues = resolveInstrumentRunValues(valueSet, variable);
+    // ONYX-1748 returning null will make a null sequence instead of an empty list of values which is consistent with the fact that there were no measures
+    if (instrumentTypeName == null || instrumentParameterCode == null) return null;
+    List<InstrumentRunValue> instrumentRunValues = resolveInstrumentRunValues(valueSet, variable);
+    if (instrumentRunValues == null || instrumentRunValues.isEmpty()) return null;
 
-      if(!instrumentRunValues.isEmpty()) {
-        List<Data> datas = new ArrayList<Data>();
+    List<Data> datas = new ArrayList<Data>();
+    for (InstrumentRunValue instrumentRunValue : instrumentRunValues) {
+      InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
 
-        for(InstrumentRunValue instrumentRunValue : instrumentRunValues) {
-          InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
+      if (instrumentType != null) {
+        InstrumentParameter instrumentParameter = instrumentType.getInstrumentParameter(instrumentParameterCode);
 
-          if(instrumentType != null) {
-            InstrumentParameter instrumentParameter = instrumentType.getInstrumentParameter(instrumentParameterCode);
-
-            if(instrumentParameter != null) {
-              datas.add(instrumentRunValue.getData(instrumentParameter.getDataType()));
-            }
-          }
+        if (instrumentParameter != null) {
+          datas.add(instrumentRunValue.getData(instrumentParameter.getDataType()));
         }
-
-        return datas;
       }
     }
-    // ONYX-1748 returning null will make a null sequence instead of an empty list of values which is consistent with the fact that there were no measures
-    return null;
+    return datas;
   }
 
   protected Object resolveContraindication(ValueSet valueSet, Variable variable) {
     String instrumentTypeName = variable.getAttributeStringValue(StageAttributeVisitor.STAGE_ATTRIBUTE);
-    if(instrumentTypeName != null) {
-      InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
-      if(instrumentType != null) {
-        InstrumentRun instrumentRun = resolveInstrumentRun(valueSet, variable);
-        if(instrumentRun != null) {
-          return instrumentType.getContraindication(instrumentRun.getContraindication());
-        }
-      }
-    }
-    return null;
+    if (instrumentTypeName == null) return null;
+
+    InstrumentType instrumentType = instrumentService.getInstrumentType(instrumentTypeName);
+    if (instrumentType == null) return null;
+
+    InstrumentRun instrumentRun = resolveInstrumentRun(valueSet, variable);
+    if (instrumentRun == null) return null;
+
+    return instrumentType.getContraindication(instrumentRun.getContraindication());
   }
 
 }
